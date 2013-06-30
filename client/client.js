7f7200841f61074b78223f91953093937b67a255
Johannes Zellner <webmaster@nebulon.de>
2013-06-29 19:34:56 -0700

    Rebuild index on filesystem changes

--- client/client.js
+++ client/client.js
@@ -4,6 +4,7 @@
 
 var dirIndex = require('../lib/dirindex');
 var optimist = require('optimist');
+var fs = require('fs');
 
 var argv = optimist.usage('Usage: $0 --root <directory>')
     .alias('h', 'help')
@@ -22,25 +23,57 @@ console.log('[II] Loading index...');
 
 var indexFileName = argv.i ? argv.i : 'index.json';
 var index = new dirIndex.DirIndex(argv.r);
+var fsWatcher;
 
-index.load(indexFileName, function(error) {
-    if (error) {
-        console.log('[WW] Unable to load index "' + indexFileName + '"', error);
-        console.log('[II] Build fresh index...');
-    }
-
-    index.build(function (error) {
+function loadIndex(callback) {
+    index.load(indexFileName, function(error) {
         if (error) {
-            console.log('[EE] Unable to build index. Nothing we can do.', error);
-            process.exit(2);
+            console.log('[WW] Unable to load index "' + indexFileName + '"', error);
+            console.log('[II] Build fresh index...');
         }
 
-        console.log('[II] Build index successfull.');
-
-        index.save(indexFileName, function (error) {
+        index.update(function (error) {
             if (error) {
-                console.log('[EE] Unable to save index to disk', error);
+                console.log('[EE] Unable to build index. Nothing we can do.', error);
+                process.exit(2);
             }
+
+            console.log('[II] Build index successfull.');
+            callback();
         });
     });
+}
+
+function saveIndex(callback) {
+    index.save(indexFileName, function (error) {
+        if (error) {
+            console.log('[EE] Unable to save index to disk', error);
+        }
+
+        callback();
+    });
+}
+
+var fsWatchTimer;
+function listenToChanges() {
+    fsWatcher = fs.watch(argv.r, {}, function (event, filename) {
+        if (!fsWatchTimer) {
+            fsWatchTimer = setTimeout(function () {
+                index.update(function (error, result) {
+                    if (error) {
+                        console.log('[EE] Unable to update the index', error);
+                        return;
+                    }
+                    console.log('[II] Index update successful');
+                });
+                fsWatchTimer = undefined;
+            }, 2000);
+        }
+    });
+}
+
+// ------ Main
+
+loadIndex(function () {
+    listenToChanges();
 });
