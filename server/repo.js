2282940259ce6e43f6094fe91ea886a5a7d6212a
Girish Ramakrishnan <girish@forwardbias.in>
2013-08-17 11:50:36 -0700

    repo: implement commit(), addFile(), updateFile(), removeFile(), createReadStream()

--- server/repo.js
+++ server/repo.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var exec = require('child_process').exec,
-    debug = require('debug')('repo.js');
+    debug = require('debug')('repo.js'),
+    path = require('path'),
+    mkdirp = require('mkdirp'),
+    fs = require('fs');
 
 exports = module.exports = {
     initialize: initialize,
@@ -10,16 +13,22 @@ exports = module.exports = {
     fileChangeTime: fileChangeTime,
     isTracked: isTracked,
     mtime: mtime,
-    commit: commit
+    commit: commit,
+    create: create,
+    addFile: addFile,
+    updateFile: updateFile,
+    removeFile: removeFile,
+    createReadStream: createReadStream
 };
 
-var gitDir;
+var gitDir, checkoutDir;
 
 function git(command, callback) {
     var options = {
-        env: { GIT_DIR: gitDir }
+        env: { GIT_DIR: gitDir },
+        cwd: checkoutDir
     };
-    debug('git ' + command);
+    debug('GIT_DIR=' + gitDir + ' git ' + command);
     exec('git ' + command, options, function (error, stdout, stderr) {
         if (error) return callback(error);
         return callback(null, stdout);
@@ -28,7 +37,12 @@ function git(command, callback) {
 
 function initialize(config, callback) {
     gitDir = config.root + '/.git';
+    checkoutDir = config.root;
 
+    updateHead(callback);
+}
+
+function updateHead(callback) {
     git('rev-parse HEAD', function (err, sha1) {
         if (err) return callback(err);
         exports.head = sha1.slice(0, -1);
@@ -36,11 +50,18 @@ function initialize(config, callback) {
     });
 }
 
+function create(options, callback) {
+    git('init', function (err) {
+        if (err) return callback(err);
+        git('config user.name ' + options.name + ' && git config user.email ' + options.email, callback);
+    });
+}
+
 function commit(commit, callback) {
-    git('show -s --pretty=%T,%ci ' + commit, function (err, out) {
+    git('show -s --pretty=%T,%ci,%P, ' + commit, function (err, out) {
         if (err) return callback(err);
         var parts = out.split(',');
-        callback(null, { treeSha1: parts[0], commitDate: new Date(parts[1])});
+        callback(null, { treeSha1: parts[0], commitDate: new Date(parts[1]), parentSha1: parts[2]});
     });
 }
 
@@ -74,7 +95,7 @@ function isTracked(file, callback) {
 
 function mtime(file, callback) {
     git('log --pretty=%ci -- ' + file, function (err, out) {
-        if (err) return callback(err);
+        if (err) return callback(null, 0);
         callback(null, new Date(out));
     });
 }
@@ -94,5 +115,53 @@ function fileChangeTime(file, fromRev, toRev, callback) {
         if (out.length == 0) return callback(null);
         callback(null, new Date(out));
     });
+}
+
+function createCommit(message, callback) {
+     git('commit -a -m \'' + message + '\'', function (err, out) {
+        if (err) return callback(err);
+        updateHead(function (err) {
+            if (err) return callback(err);
+            commit(exports.head, callback);
+        });
+     });
+}
+
+function addFile(file, options, callback) {
+    // FIXME: ensure this is a new file
+    if (!options.message) options.message = 'Adding file ' + file;
+    mkdirp(path.dirname(file), function (ignoredErr) {
+        fs.rename(options.path, path.join(checkoutDir, file), function (err) {
+            if (err) return callback(err);
+            git('add ' + file, function (err) {
+                if (err) return callback(err);
+                createCommit(options.message, callback);
+            });
+        });
+    });
+}
+
+function updateFile(file, options, callback) {
+    // FIXME: ensure this is an updated file
+    if (!options.message) options.message = 'Updating file ' + file;
+    fs.rename(options.path, path.join(checkoutDir, file), function (err) {
+        if (err) return callback(err);
+        git('add ' + file, function (err) {
+            if (err) return callback(err);
+            createCommit(options.message, callback);
+        });
+    });
+}
+
+function removeFile(file, callback) {
+    var message = 'Removing file ' + file;
+    fs.unlink(path.join(checkoutDir, file), function (err) {
+        if (err) return callback(err);
+        createCommit(message, callback);
+    });
+}
+
+function createReadStream(file) {
+    return fs.createReadStream(path.join(checkoutDir, file));
 }
 
