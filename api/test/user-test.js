b7dfcc8576ae79a1bc8ce2bd16c03f408269b93a
Johannes Zellner <webmaster@nebulon.de>
2013-09-05 08:31:16 -0700

    Add initial user.js to split up the user route and API

---
+++ api/test/user-test.js
@@ -0,0 +1,169 @@
+'use strict';
+
+/* global it:false */
+/* global describe:false */
+/* global before:false */
+/* global after:false */
+
+var user = require('../user.js'),
+    UserError = user.UserError,
+    os = require('os'),
+    path = require('path'),
+    mkdirp = require('mkdirp'),
+    crypto = require('crypto'),
+    rimraf = require('rimraf'),
+    db = require('../database.js');
+
+var assert = require('assert');
+var expect = require('expect.js');
+
+var USERNAME = 'nobody';
+var EMAIL = 'nobody@no.body';
+var PASSWORD = 'foobar';
+
+var tmpdirname = 'user-test-' + crypto.randomBytes(4).readUInt32LE(0);
+var config = {
+    port: 3000,
+    dataRoot: path.resolve(os.tmpdir(), tmpdirname),
+    configRoot: path.resolve(os.tmpdir(), tmpdirname),
+    mountRoot: path.resolve(os.tmpdir(), tmpdirname)
+};
+
+function cleanupUser(done) {
+    user.remove(USERNAME, PASSWORD, function () {
+        done();
+    });
+}
+
+function createUser(done) {
+    user.create(USERNAME, PASSWORD, EMAIL, {}, function (error, result) {
+        done();
+    });
+}
+
+function setupDatabase(done) {
+    // ensure data/config/mount paths
+    mkdirp.sync(config.dataRoot);
+    mkdirp.sync(config.configRoot);
+    mkdirp.sync(config.mountRoot);
+
+    db.initialize(config);
+
+    done();
+}
+
+function cleanupDatabase(done) {
+    rimraf(config.dataRoot, function (error) {
+        rimraf(config.configRoot, function (error) {
+            rimraf(config.mountRoot, function (error) {
+                done();
+            });
+        });
+    });
+}
+
+describe('User', function () {
+    before(setupDatabase);
+    after(cleanupDatabase);
+
+    describe('create', function() {
+        before(cleanupUser);
+        after(cleanupUser);
+
+        it('succeeds', function (done) {
+            user.create(USERNAME, PASSWORD, EMAIL, {}, function (error, result) {
+                expect(error).not.to.be.ok();
+                expect(result).to.be.ok();
+                expect(result.username).to.equal(USERNAME);
+                expect(result.email).to.equal(EMAIL);
+
+                done();
+            });
+        });
+
+        it('fails because of invalid arguments', function (done) {
+            expect(function () {
+                user.create(EMAIL, {}, function () {});
+            }).to.throwException();
+            expect(function () {
+                user.create(12345, PASSWORD, EMAIL, {}, function () {});
+            }).to.throwException();
+            expect(function () {
+                user.create(USERNAME, PASSWORD, EMAIL, {});
+            }).to.throwException();
+            expect(function () {
+                user.create(USERNAME, PASSWORD, EMAIL, function () {});
+            }).to.throwException();
+            expect(function () {
+                user.create(USERNAME, PASSWORD, EMAIL, {}, {});
+            }).to.throwException();
+
+            done();
+        });
+
+        it('fails because user exists', function (done) {
+            user.create(USERNAME, PASSWORD, EMAIL, {}, function (error, result) {
+                expect(error).to.be.ok();
+                expect(result).not.to.be.ok();
+                expect(error.reason).to.equal(UserError.ALREADY_EXISTS);
+
+                done();
+            });
+        });
+
+        it('fails because password is empty', function (done) {
+            user.create(USERNAME, '', EMAIL, {}, function (error, result) {
+                expect(error).to.be.ok();
+                expect(result).not.to.be.ok();
+                expect(error.reason).to.equal(UserError.ARGUMENTS);
+
+                done();
+            });
+        });
+    });
+
+    describe("verify", function () {
+        before(createUser);
+        after(cleanupUser);
+
+        it('fails due to non existing username', function (done) {
+            user.verify(USERNAME+USERNAME, PASSWORD, function (error, result) {
+                expect(error).to.be.ok();
+                expect(result).to.not.be.ok();
+                expect(error.reason).to.equal(UserError.NOT_FOUND);
+
+                done();
+            });
+        });
+
+        it('fails due to empty password', function (done) {
+            user.verify(USERNAME, '', function (error, result) {
+                expect(error).to.be.ok();
+                expect(result).to.not.be.ok();
+                expect(error.reason).to.equal(UserError.ARGUMENTS);
+
+                done();
+            });
+        });
+
+        it('fails due to wrong password', function (done) {
+            user.verify(USERNAME, PASSWORD+PASSWORD, function (error, result) {
+                expect(error).to.be.ok();
+                expect(result).to.not.be.ok();
+                expect(error.reason).to.equal(UserError.WRONG_USER_OR_PASSWORD);
+
+                done();
+            });
+        });
+
+        it('succeeds', function (done) {
+            user.verify(USERNAME, PASSWORD, function (error, result) {
+                expect(error).to.not.be.ok();
+                expect(result).to.be.ok();
+
+                done();
+            });
+        });
+    });
+});
+
---
+++ api/user.js
@@ -0,0 +1,166 @@
+'use strict';
+
+var db = require('./database.js'),
+    DatabaseError = db.DatabaseError,
+    crypto = require('crypto'),
+    util = require('util'),
+    debug = require('debug')('user.js'),
+    assert = require('assert');
+
+exports = module.exports = {
+    UserError: UserError,
+
+    create: createUser,
+    verify: verifyUser,
+    remove: removeUser,
+    changePassword: changePassword,
+    update: updateUser,
+};
+
+var CRYPTO_SALT_SIZE = 64; // 512-bit salt
+var CRYPTO_ITERATIONS = 10000; // iterations
+var CRYPTO_KEY_LENGTH = 512; // bits
+
+// http://dustinsenos.com/articles/customErrorsInNode
+// http://code.google.com/p/v8/wiki/JavaScriptStackTraceApi
+function UserError(err, reason) {
+    Error.call(this);
+    Error.captureStackTrace(this, this.constructor);
+
+    this.name = this.constructor.name;
+    this.message = JSON.stringify(err);
+    this.code = err ? err.code : null;
+    this.reason = reason || UserError.INTERNAL_ERROR;
+}
+util.inherits(UserError, Error);
+UserError.DATABASE_ERROR = 1;
+UserError.INTERNAL_ERROR = 2;
+UserError.ALREADY_EXISTS = 3;
+UserError.NOT_FOUND = 4;
+UserError.WRONG_USER_OR_PASSWORD = 5;
+UserError.ARGUMENTS = 6;
+
+function ensureArgs(args, expected) {
+    assert(args.length === expected.length);
+
+    for (var i = 0; i < args.length; ++i) {
+        if (expected[i]) {
+            assert(typeof args[i] === expected[i]);
+        }
+    }
+}
+
+function createUser(username, password, email, options, callback) {
+    ensureArgs(arguments, ['string', 'string', 'string', 'object', 'function']);
+
+    if (username.length === 0) {
+        return callback(new UserError('username empty', UserError.ARGUMENTS));
+    }
+
+    if (password.length === 0) {
+        return callback(new UserError('password empty', UserError.ARGUMENTS));
+    }
+
+    if (email.length === 0) {
+        return callback(new UserError('email empty', UserError.ARGUMENTS));
+    }
+
+    crypto.randomBytes(CRYPTO_SALT_SIZE, function (error, salt) {
+        if (error) {
+            return callback(new UserError('Failed to generate random bytes', UserError.INTERNAL_ERROR));
+        }
+
+        crypto.pbkdf2(password, salt, CRYPTO_ITERATIONS, CRYPTO_KEY_LENGTH, function (error, derivedKey) {
+            if (error) {
+                return callback(new UserError('Failed to hash password', UserError.INTERNAL_ERROR));
+            }
+
+            var now = (new Date()).toUTCString();
+            var user = {
+                username: username,
+                email: email,
+                password: new Buffer(derivedKey, 'binary').toString('hex'),
+                salt: salt.toString('hex'),
+                created_at: now,
+                updated_at: now
+            };
+
+            db.USERS_TABLE.put(user, function (error) {
+                if (error) {
+                    if (error.reason === DatabaseError.ALREADY_EXISTS) {
+                        return callback(new UserError('Already exists', UserError.ALREADY_EXISTS));
+                    }
+                    return callback(error);
+                }
+
+                callback(null, {username: username, email: email});
+            });
+        });
+    });
+}
+
+function verifyUser(username, password, callback) {
+    ensureArgs(arguments, ['string', 'string', 'function']);
+
+    if (username.length === 0) {
+        return callback(new UserError('username empty', UserError.ARGUMENTS));
+    }
+
+    if (password.length === 0) {
+        return callback(new UserError('password empty', UserError.ARGUMENTS));
+    }
+
+    db.USERS_TABLE.get(username, function (error, user) {
+        if (error) {
+            if (error.reason === DatabaseError.NOT_FOUND) {
+                return callback(new UserError('Username not found', UserError.NOT_FOUND));
+            }
+
+            return callback(error);
+        }
+
+        var saltBinary = new Buffer(user.salt, 'hex');
+        crypto.pbkdf2(password, saltBinary, CRYPTO_ITERATIONS, CRYPTO_KEY_LENGTH, function (error, derivedKey) {
+            if (error) {
+                return callback(new UserError('Failed to hash password', UserError.INTERNAL_ERROR));
+            }
+
+            var derivedKeyHex = new Buffer(derivedKey, 'binary').toString('hex');
+            if (derivedKeyHex != user.password)  {
+                return callback(new UserError('Username and password does not match', UserError.WRONG_USER_OR_PASSWORD));
+            }
+
+            callback(null, { username: user.username, email: user.email });
+        });
+    });
+}
+
+function removeUser(username, password, callback) {
+    ensureArgs(arguments, ['string', 'string', 'function']);
+
+    verifyUser(username, password, function (error, result) {
+        if (error) {
+            return callback(error);
+        }
+
+        db.USERS_TABLE.remove(username, function (error, user) {
+            if (error) {
+                return callback(error);
+            }
+
+            callback(null, user);
+        });
+    });
+}
+
+function updateUser(username, password, options, callback) {
+    ensureArgs(arguments, ['string', 'string', 'object', 'function']);
+
+    callback(new UserError('not implemented', UserError.INTERNAL_ERROR));
+}
+
+function changePassword(username, oldPassword, newPassword, callback) {
+    ensureArgs(arguments, ['string', 'string', 'string', 'function']);
+
+    callback(new UserError('not implemented', UserError.INTERNAL_ERROR));
+}
