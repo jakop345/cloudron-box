1618e42e5d87ea5f2ba57fbfafce3c037a2c245d
Girish Ramakrishnan <girish@forwardbias.in>
2013-10-23 20:56:03 -0700

    Fix linting errors

--- middleware/contentType.js
+++ middleware/contentType.js
@@ -1,7 +1,8 @@
+'use strict';
+
 module.exports = function contentType(type) {
     return function (req, res, next) {
         res.setHeader('Content-Type', type);
         next();
-    }
-}
-
+    };
+};
