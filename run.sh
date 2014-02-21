74ddb991a6d1818050ba232e34948f8ecdc0d08e
Johannes Zellner <webmaster@nebulon.de>
2014-02-21 08:38:15 -0800

    Add initial nginx configuration
    This adds basic configuration for 80 and 443
    nginx can be run with:
    ./run.sh
    and requires root access to bind to the ports.
    
    Basic routes are specified in nginx/server.routes
    Additional application routes can be put into the
    nginx/applications/ subfolder.

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo
+echo "Starting YellowTent router..."
+echo
+
+#### When using it as a future start suite
+# if [[ `whoami` == root ]]; then
+#     echo "Do not run the script as root!"
+#     echo "This script spawns nginx with sudo as well as unprivileged servers."
+#     echo
+#     exit 1;
+# fi
+
+cd nginx
+sudo nginx -c yellowtent.conf -p $PWD
+cd ..
