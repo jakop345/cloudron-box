9ef40b2dcfc8594ae97f28085f8a5e73bac92a89
Girish Ramakrishnan <girish@forwardbias.in>
2014-04-07 16:43:06 -0700

    Rename volume server separately at port 3500
    The top level volumeserver file now run the volume server at 3500.
    All api calls /api/v1/volume/* are routed to this server by nginx.

---
+++ volume/routes/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+exports = module.exports = {
+    user: require('../../auth/routes/user.js'),
+    volume: require('./volume.js')
+};
+
--- volume/routes/test/volume-test.js
+++ volume/routes/test/volume-test.js
@@ -5,7 +5,7 @@
 /* global before:false */
 /* global after:false */
 
-var Server = require('../../../sync/server.js'),
+var Server = require('../../server.js'),
     request = require('superagent'),
     expect = require('expect.js'),
     database = require('../../../auth/database.js'),
