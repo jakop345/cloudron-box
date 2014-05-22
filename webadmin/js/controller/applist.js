28ed75d3e0c3716b16cb5803ceb0a324b36090f6
Girish Ramakrishnan <girish@forwardbias.in>
2014-05-21 18:47:28 -0700

    Show loading status for apps

--- webadmin/js/controller/applist.js
+++ webadmin/js/controller/applist.js
@@ -3,14 +3,26 @@
 var AppListController = function ($scope, $http, $location, config) {
     console.debug('AppListController');
 
+    $scope.LOADING = 1;
+    $scope.ERROR = 2;
+    $scope.LOADED = 3;
+
+    $scope.loadStatus = $scope.LOADED;
+    $scope.loadError = '';
+
     $scope.refresh = function () {
+        $scope.loadStatus = $scope.LOADING;
+
         $http.get(config.APPSTORE_URL + '/api/v1/apps')
             .success(function (data, status, headers) {
                 console.log(data);
                 data.apps.forEach(function (app) { app.iconUrl = config.APPSTORE_URL + "/api/v1/app/" + app.id + "/icon"; });
                 $scope.apps = data.apps;
+                $scope.loadStatus = $scope.LOADED;
             }).error(function (data, status, headers) {
-                console.log('error in getting app list');
+                $scope.loadStatus = $scope.ERROR;
+                $scope.loadError = status + '';
+                console.log('error in getting app list', data, status);
             });
     };
 
