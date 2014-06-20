48c6c098e34cfca5f82164d95641eccba21f1e8b
Girish Ramakrishnan <girish@forwardbias.in>
2014-06-19 20:49:25 -0700

    Move config variables to config.js
    This way they can be required by various modules as needed. Instead
    of passing them around everywhere like we do now.

--- app.js
+++ app.js
@@ -9,49 +9,8 @@ if (typeof process.env.NODE_ENV === 'undefined') {
 
 var Server = require('./src/server.js'),
     path = require('path'),
-    os = require('os');
-
-function getUserHomeDir() {
-    return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
-}
-
-var baseDir = path.join(getUserHomeDir(), '.yellowtent');
-
-var appDataRoot = path.join(baseDir, 'appdata');
-var configRoot = path.join(baseDir, 'config');
-var dataRoot = path.join(baseDir, 'data');
-var mountRoot = path.join(baseDir, 'mount');
-var port = 3000;
-var silent = false;
-var nginxAppConfigDir = path.join(__dirname, 'nginx/applications/');
-
-// load provisioned config file if there
-var configFile;
-try {
-    configFile = require('/etc/yellowtent.json');
-} catch (e) {
-    console.log('Unable to load provisioned config file. Using defaults.');
-    configFile = {
-        token: null,
-        appstoreOrigin: 'https://selfhost.io:5050',
-        origin: 'https://' + os.hostname()
-    };
-}
-
-// main entry point when running standalone
-// TODO Maybe this should go into a new 'executeable' file - Johannes
-var config = {
-    port: port,
-    dataRoot: dataRoot,
-    configRoot: configRoot,
-    mountRoot: mountRoot,
-    silent: silent,
-    token: configFile.token,
-    appServerUrl: configFile.appstoreOrigin,
-    origin: configFile.origin,
-    nginxAppConfigDir: nginxAppConfigDir,
-    appDataRoot: appDataRoot
-};
+    os = require('os'),
+    config = require('./config.js');
 
 var server = new Server(config);
 server.start(function (err) {
---
+++ config.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var path = require('path'),
+    os = require('os');
+
+function getUserHomeDir() {
+    return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+}
+
+var baseDir = path.join(getUserHomeDir(), '.yellowtent');
+
+var appDataRoot = path.join(baseDir, 'appdata');
+var configRoot = path.join(baseDir, 'config');
+var dataRoot = path.join(baseDir, 'data');
+var mountRoot = path.join(baseDir, 'mount');
+var port = 3000;
+var silent = false;
+var nginxAppConfigDir = path.join(__dirname, 'nginx/applications/');
+
+// load provisioned config file if there
+var configFile;
+try {
+    configFile = require('/etc/yellowtent.json');
+} catch (e) {
+    console.log('Unable to load provisioned config file. Using defaults.');
+    configFile = {
+        token: null,
+        appstoreOrigin: 'https://selfhost.io:5050',
+        origin: 'https://' + os.hostname()
+    };
+}
+
+exports = module.exports = {
+    port: port,
+    dataRoot: dataRoot,
+    configRoot: configRoot,
+    mountRoot: mountRoot,
+    silent: silent,
+    token: configFile.token,
+    appServerUrl: configFile.appstoreOrigin,
+    origin: configFile.origin,
+    nginxAppConfigDir: nginxAppConfigDir,
+    appDataRoot: appDataRoot
+};
+
