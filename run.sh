1257ad2877db93c1d85f65b4879a82391ecb435e
Girish Ramakrishnan <girish@forwardbias.in>
2014-06-25 08:16:42 -0700

    Remove dead code

--- run.sh
+++ run.sh
@@ -5,13 +5,6 @@ echo "Starting YellowTent server at port 443..."
 echo
 
 BASEDIR=$(dirname $0)
-#### When using it as a future start suite
-# if [[ `whoami` == root ]]; then
-#     echo "Do not run the script as root!"
-#     echo "This script spawns nginx with sudo as well as unprivileged servers."
-#     echo
-#     exit 1;
-# fi
 
 sudo mkdir -p /var/log/supervisord
 sudo NGINX_ROOT=$BASEDIR supervisord -n -c supervisor/supervisord.conf
