e6a772e35b5fbd182345b522c2b9534e363978af
Girish Ramakrishnan <girish@forwardbias.in>
2014-06-28 18:03:41 -0700

    create naked_domain.conf in run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ BASEDIR=$(dirname $0)
 
 # Fix the hostname for the admin application
 sed -e "s/##ADMIN_HOSTNAME##/admin.$HOSTNAME/" nginx/admin.conf_template > nginx/applications/admin.conf
+touch nginx/naked_domain.conf
 
 sudo mkdir -p /var/log/supervisord
 sudo NGINX_ROOT=$BASEDIR supervisord -n -c supervisor/supervisord.conf
