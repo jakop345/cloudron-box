3437474837c3443b294f95bcfdbdeb219ac9345c
Johannes Zellner <webmaster@nebulon.de>
2014-07-09 10:59:05 -0700

    Make the announce init script retry until it succeeds

--- scripts/initializeBaseUbuntuImage.sh
+++ scripts/initializeBaseUbuntuImage.sh
@@ -99,10 +99,22 @@ echo "==== Install init script ===="
 cat > /etc/init.d/bootstrap <<EOF
 #!/bin/sh
 
-curl -v https://appstore-dev.herokuapp.com/api/v1/boxes/announce?name=\`hostname -f\` >> /tmp/yellowtent
-# curl -v https://nebulon.fwd.wf/api/v1/boxes/announce?name=\`hostname -f\` >> /tmp/yellowtent
+OUT="/tmp/bootstrap"
+STDOUT="$OUT.log"
 
-echo "box announced itself to appstore" >> /tmp/yellowtent
+ANNOUNCE="curl -fv https://appstore-dev.herokuapp.com/api/v1/boxes/announce?name=`hostname -f` &>> $STDOUT"
+#ANNOUNCE="curl -fv https://nebulon.fwd.wf/api/v1/boxes/announce?name=`hostname -f` &>> $STDOUT"
+
+eval $ANNOUNCE
+RET=$?
+while [[ $RET -ne 0 ]]; do
+        echo "[EE] Failed to announce itself with error code $RET, try again in a second" >> $OUT
+        sleep 1
+        eval $ANNOUNCE
+        RET=$?
+done
+
+echo "[II] Successfully announced itself to appstore" >> $OUT
 EOF
 chmod +x /etc/init.d/bootstrap
 update-rc.d bootstrap defaults
