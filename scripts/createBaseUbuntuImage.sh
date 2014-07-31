3919bce140763350fd5ba9edb6d0ff88a2be34ed
Johannes Zellner <webmaster@nebulon.de>
2014-07-31 09:31:03 -0700

    Ignore known hosts warnings for creating base images

--- scripts/createBaseUbuntuImage.sh
+++ scripts/createBaseUbuntuImage.sh
@@ -11,6 +11,6 @@ echo "Creating base image using droplet with IP $1";
 
 cd $BASEDIR/..
 
-scp -i ../appstore/ssh/id_rsa_yellowtent ./scripts/initializeBaseUbuntuImage.sh root@$1:.
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ../appstore/ssh/id_rsa_yellowtent ./scripts/initializeBaseUbuntuImage.sh root@$1:.
 
-ssh -i ../appstore/ssh/id_rsa_yellowtent root@$1 "/bin/bash /root/initializeBaseUbuntuImage.sh"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ../appstore/ssh/id_rsa_yellowtent root@$1 "/bin/bash /root/initializeBaseUbuntuImage.sh"
