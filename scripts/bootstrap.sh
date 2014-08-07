09a7e6244bc195267a9db4d37fdc379dc036c961
Girish Ramakrishnan <girish@forwardbias.in>
2014-08-07 06:12:45 -0700

    Rename BASEDIR to SRCDIR

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "Box bootstrapping"
 
-BASEDIR=/home/yellowtent/box
 USER=yellowtent
+SRCDIR=/home/$USER/box
 
 # we get the appstore origin from the caller which is baked into the image
 APP_SERVER_URL=$1
@@ -18,32 +18,32 @@ echo "==== Setup ssl certs ===="
 CERTIFICATE_DIR=/etc/yellowtent/cert
 mkdir -p $CERTIFICATE_DIR
 cd $CERTIFICATE_DIR
-/bin/bash $BASEDIR/scripts/generate_certificate.sh US California 'San Francisco' Selfhost Cloudron `hostname -f` cert@selfhost.io .
+/bin/bash $SRCDIR/scripts/generate_certificate.sh US California 'San Francisco' Selfhost Cloudron `hostname -f` cert@selfhost.io .
 tar xf cert.tar
 
 
 echo "==== Sudoers file for app removal ===="
 cat > /etc/sudoers.d/yellowtent <<EOF
-Defaults!$BASEDIR/src/rmappdir.sh env_keep=HOME
-$USER ALL=(root) NOPASSWD: $BASEDIR/src/rmappdir.sh
+Defaults!$SRCDIR/src/rmappdir.sh env_keep=HOME
+$USER ALL=(root) NOPASSWD: $SRCDIR/src/rmappdir.sh
 
-Defaults!$BASEDIR/src/reloadnginx.sh env_keep=HOME
-$USER ALL=(root) NOPASSWD: $BASEDIR/src/reloadnginx.sh
+Defaults!$SRCDIR/src/reloadnginx.sh env_keep=HOME
+$USER ALL=(root) NOPASSWD: $SRCDIR/src/reloadnginx.sh
 EOF
 
 
 echo "==== Setup yellowtent ===="
-cd $BASEDIR
+cd $SRCDIR
 npm install --production
 
 
 echo "==== Setup nginx ===="
-cd $BASEDIR
+cd $SRCDIR
 killall nginx || echo "nginx not running"   # condition makes killall not fatal to set -e
 mkdir -p /home/$USER/.yellowtent/applications
-ln -sf /home/$USER/.yellowtent/applications $BASEDIR/nginx/applications
+ln -sf /home/$USER/.yellowtent/applications $SRCDIR/nginx/applications
 touch /home/$USER/.yellowtent/naked_domain.conf
-ln -sf /home/$USER/.yellowtent/naked_domain.conf $BASEDIR/nginx/naked_domain.conf
+ln -sf /home/$USER/.yellowtent/naked_domain.conf $SRCDIR/nginx/naked_domain.conf
 FQDN=`hostname -f`
 sed -e "s/##ADMIN_FQDN##/admin-$FQDN/" nginx/admin.conf_template > nginx/applications/admin.conf
 # TODO until I find a way to have a more dynamic nginx config
@@ -57,12 +57,12 @@ supervisorctl shutdown || echo "supervisord not running"
 rm -rf /etc/supervisor
 mkdir -p /etc/supervisor
 mkdir -p /etc/supervisor/conf.d
-cp $BASEDIR/supervisor/supervisord.conf /etc/supervisor/
+cp $SRCDIR/supervisor/supervisord.conf /etc/supervisor/
 
 echo "Writing box supervisor config..."
 cat > /etc/supervisor/conf.d/nginx.conf <<EOF
 [program:nginx]
-command=nginx -c nginx.conf -p $BASEDIR/nginx/
+command=nginx -c nginx.conf -p $SRCDIR/nginx/
 autostart=true
 autorestart=true
 redirect_stderr=true
@@ -76,7 +76,7 @@ command=node app.js
 autostart=true
 autorestart=true
 redirect_stderr=true
-directory=$BASEDIR
+directory=$SRCDIR
 user=yellowtent
 environment=HOME="/home/yellowtent",USER="yellowtent",DEBUG="box*",APP_SERVER_URL=$APP_SERVER_URL
 EOF
