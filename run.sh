jakop345/cloudron-box

commit 6fe11f869a91dd2f81e590227907f7d7f241d27f

Do not depend on os.hostname() for fqdn
The non provisioned boxes will always be configured
to work on localhost. This includes the webadmin OAuth
records. After the cloudron gets provisioned, the FQDN
will be set to the correct value, which requires regeneration
of the OAuth client records.

run.sh
@@ -5,12 +5,14 @@ echo "Starting YellowTent server at port 443..."
 echo
 
 SRCDIR="$(cd $(dirname "$0"); pwd)"
+NGINX_ROOT=~/.yellowtent/nginx
 
-# Fix the hostname for the admin application
-FQDN=`hostname -f`
-sed -e "s/##ADMIN_FQDN##/admin-$FQDN/" -e "s|##SRCDIR##|$SRCDIR|" nginx/admin.conf_template > nginx/applications/admin.conf
+mkdir -p $NGINX_ROOT
+
 touch nginx/naked_domain.conf
 
+cp -rf $SRCDIR/nginx/* $NGINX_ROOT/
+
 sudo mkdir -p /var/log/supervisor
-sudo NGINX_ROOT=$SRCDIR supervisord -n -c supervisor/supervisord.conf
+sudo NGINX_ROOT=$NGINX_ROOT supervisord -n -c supervisor/supervisord.conf
 

config.js
@@ -55,7 +55,7 @@ config.save = function () {
 
     config.nginxAppConfigDir = path.join(config.nginxConfigDir, 'applications');
 
-    config.fqdn = process.env.FQDN || os.hostname();
+    config.fqdn = 'localhost';
     config.adminOrigin = 'https://admin-' + config.fqdn;
 
     config.token = null;

src/server.js
@@ -22,6 +22,7 @@ var express = require('express'),
     apps = require('./apps'),
     middleware = require('./middleware'),
     database = require('./database.js'),
+    clientdb = require('./clientdb.js'),
     userdb = require('./userdb'),
     config = require('../config.js'),
     _ = require('underscore');
@@ -169,19 +170,26 @@ Server.prototype._provision = function (req, res, next) {
 
     config.set(_.pick(req.body, 'token', 'appServerUrl', 'adminOrigin', 'fqdn'));
 
-    next(new HttpSuccess(201, {}));
+    // override the default webadmin OAuth client record
+    clientdb.del('webadmin', function () {
+        clientdb.add('webadmin', 'cid-webadmin', 'unused', 'WebAdmin', config.adminOrigin, function (error) {
+            if (error) return next(new HttpError(500, error));
 
-    // now try to get the real certificate
-    function getCertificateCallback(error) {
-        if (error) {
-            console.error(error);
-            return setTimeout(that._getCertificate.bind(that, getCertificateCallback), 5000);
-        }
+            next(new HttpSuccess(201, {}));
 
-        debug('_provision: success');
-    }
+            // now try to get the real certificate
+            function getCertificateCallback(error) {
+                if (error) {
+                    console.error(error);
+                    return setTimeout(that._getCertificate.bind(that, getCertificateCallback), 5000);
+                }
 
-    this._getCertificate(getCertificateCallback);
+                debug('_provision: success');
+            }
+
+            this._getCertificate(getCertificateCallback);
+        });
+    });
 };
 
 /*

supervisor/conf.d/nginx.conf
@@ -1,4 +1,4 @@
 [program:nginx]
-command=nginx -c nginx.conf -p %(ENV_NGINX_ROOT)s/nginx/
+command=nginx -c nginx.conf -p %(ENV_NGINX_ROOT)s/
 autostart=true
 autorestart=true

commit 79a06beb4f85a66f33341efd6ab7a6266431b461

generate admin.conf from template
6fe11f869a91dd2 broke it probably

run.sh
@@ -12,6 +12,7 @@ mkdir -p $NGINX_ROOT
 touch nginx/naked_domain.conf
 
 cp -rf $SRCDIR/nginx/* $NGINX_ROOT/
+sed -e "s/##ADMIN_FQDN##/admin-localhost/" -e "s|##SRCDIR##|$SRCDIR|" nginx/admin.conf_template > $NGINX_ROOT/applications/admin.conf
 
 sudo mkdir -p /var/log/supervisor
 sudo NGINX_ROOT=$NGINX_ROOT supervisord -n -c supervisor/supervisord.conf
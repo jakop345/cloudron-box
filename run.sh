79a06beb4f85a66f33341efd6ab7a6266431b461
Girish Ramakrishnan <girish@forwardbias.in>
2014-08-20 00:45:28 -0700

    generate admin.conf from template
    6fe11f869a91dd2 broke it probably

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ mkdir -p $NGINX_ROOT
 touch nginx/naked_domain.conf
 
 cp -rf $SRCDIR/nginx/* $NGINX_ROOT/
+sed -e "s/##ADMIN_FQDN##/admin-localhost/" -e "s|##SRCDIR##|$SRCDIR|" nginx/admin.conf_template > $NGINX_ROOT/applications/admin.conf
 
 sudo mkdir -p /var/log/supervisor
 sudo NGINX_ROOT=$NGINX_ROOT supervisord -n -c supervisor/supervisord.conf
