525aa523ff6952caf6be737f3635965ee312de0f
Girish Ramakrishnan <girish@forwardbias.in>
2014-08-20 00:49:40 -0700

    Copy over nginx setup code from scripts/bootstrap.sh
    Copying nginx/* from source repo copies over a lots of other files

--- run.sh
+++ run.sh
@@ -9,10 +9,18 @@ NGINX_ROOT=~/.yellowtent/nginx
 
 mkdir -p $NGINX_ROOT
 
-touch nginx/naked_domain.conf
 
-cp -rf $SRCDIR/nginx/* $NGINX_ROOT/
+# keep this in sync with scripts/bootstrap.sh
+mkdir -p $NGINX_ROOT/applications
+mkdir -p $NGINX_ROOT/cert
+
+cp nginx/nginx.conf $NGINX_ROOT/nginx.conf
+cp nginx/mime.types $NGINX_ROOT/mime.types
+cp nginx/certificates.conf $NGINX_ROOT/certificates.conf
+
+touch $NGINX_ROOT/naked_domain.conf
 sed -e "s/##ADMIN_FQDN##/admin-localhost/" -e "s|##SRCDIR##|$SRCDIR|" nginx/admin.conf_template > $NGINX_ROOT/applications/admin.conf
+
 
 sudo mkdir -p /var/log/supervisor
 sudo NGINX_ROOT=$NGINX_ROOT supervisord -n -c supervisor/supervisord.conf
