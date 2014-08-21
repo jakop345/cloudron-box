ac51ab46fdfe689af2b4f3bf23ea74d37cd3a947
Johannes Zellner <webmaster@nebulon.de>
2014-08-21 15:40:02 -0700

    webadmin: remove unused splash view

--- webadmin/views/splash.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var SplashController = function ($scope, Client, Spinner) {
-    var spinner = new Spinner().spin();
-    spinner.el.style.left = '50%';
-    spinner.el.style.top = '50%';
-    document.getElementById('spinner-container').appendChild(spinner.el);
-};
