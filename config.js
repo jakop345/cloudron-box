cefc7736dbda0b8539e05fbff241e9b2e862494f
Girish Ramakrishnan <girish@forwardbias.in>
2014-09-03 19:51:37 -0700

    Release 0.2.0

--- config.js
+++ config.js
@@ -62,7 +62,7 @@ config.save = function () {
 
     config.token = null;
     config.nakedDomain = null;
-    config.version = '0.2.0';
+    config.version = '0.3.0';
     config.aws = null;
 
     mkdirp.sync(config.baseDir);
