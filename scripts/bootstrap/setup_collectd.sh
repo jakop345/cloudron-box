62cb58b5b1c49bda73d4e54b5514632159cdd413
Girish Ramakrishnan <girish@forwardbias.in>
2014-09-16 08:05:25 -0700

    Run docker in background

--- scripts/bootstrap/setup_collectd.sh
+++ scripts/bootstrap/setup_collectd.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run -p 127.0.0.1:2003:2003 -p 127.0.0.1:2004:2004 -p 127.0.0.1:8000:8000 --name="graphite" girish/graphite:0.1
+docker run -d -p 127.0.0.1:2003:2003 -p 127.0.0.1:2004:2004 -p 127.0.0.1:8000:8000 --name="graphite" girish/graphite:0.1
 
 # collectd
 cp $SCRIPT_DIR/collectd/collectd.conf /etc/collectd/collectd.conf
