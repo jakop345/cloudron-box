d5ac037ffc1af10db6507a03bac4ebbbddf75c94
Girish Ramakrishnan <girish@forwardbias.in>
2014-09-16 19:06:29 -0700

    bump the graphite docker version

--- scripts/bootstrap/setup_collectd.sh
+++ scripts/bootstrap/setup_collectd.sh
@@ -9,7 +9,7 @@ docker run -d --name="graphite" \
     -p 127.0.0.1:2003:2003 \
     -p 127.0.0.1:2004:2004 \
     -p 127.0.0.1:8000:8000 \
-    -v $GRAPHITE_DIR:/app/data girish/graphite:0.1
+    -v $GRAPHITE_DIR:/app/data girish/graphite:0.2
 
 # collectd
 cp $SCRIPT_DIR/collectd/collectd.conf /etc/collectd/collectd.conf
