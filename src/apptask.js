4ba5967746891fbc750508954fe4166fad0c5f67
Girish Ramakrishnan <girish@forwardbias.in>
2014-09-28 13:17:51 -0700

    Export MAIL_SERVER env variable for apps

--- src/apptask.js
+++ src/apptask.js
@@ -196,6 +196,7 @@ function createContainer(app, callback) {
 
         env.push('APP_ORIGIN' + '=' + 'https://' + appFqdn(app.location));
         env.push('ADMIN_ORIGIN' + '=' + config.adminOrigin);
+        env.push('MAIL_SERVER' + '=' + config.mailServer);
 
         // add oauth variables
         clientdb.getByAppId(app.id, function (error, client) {
