920fe2df6f03e3d00fa36c41a27a1850e2dd764c
Girish Ramakrishnan <girish@forwardbias.in>
2014-09-27 23:13:13 -0700

    Fix haraka setup script

--- scripts/bootstrap/setup_haraka.sh
+++ scripts/bootstrap/setup_haraka.sh
@@ -2,17 +2,16 @@
 
 set -e
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}/.." )" && pwd )"
 DOMAIN_NAME=`hostname -f`
 
 HARAKA_DIR="/home/yellowtent/.yellowtent/haraka"
 DKIM_DIR="$HARAKA_DIR/dkim/$DOMAIN_NAME"
 mkdir -p $DKIM_DIR
 openssl genrsa -out $DKIM_DIR/${DOMAIN_NAME}.private 1024
-openssl rsa -in $DKIM_DIR/${DOMAIN_NAME}.private -out $DKIM_DIR/{DOMAIN_NAME}.public -pubout -outform PEM
+openssl rsa -in $DKIM_DIR/${DOMAIN_NAME}.private -out $DKIM_DIR/${DOMAIN_NAME}.public -pubout -outform PEM
 
 docker run -d --name="haraka" \
-    -p 127.0.0.1:25:25
+    -p 127.0.0.1:25:25 \
     -v $HARAKA_DIR:/app/data girish/haraka:0.1
 
 chown -R yellowtent.yellowtent $HARAKA_DIR
