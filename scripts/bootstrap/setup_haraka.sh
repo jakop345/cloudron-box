144fde9e4357fb2cf885f1ce514ca77f80051c4e
Girish Ramakrishnan <girish@forwardbias.in>
2014-09-27 23:51:37 -0700

    Set DOMAIN_NAME env variable in the haraka container

--- scripts/bootstrap/setup_haraka.sh
+++ scripts/bootstrap/setup_haraka.sh
@@ -12,6 +12,8 @@ openssl rsa -in $DKIM_DIR/${DOMAIN_NAME}.private -out $DKIM_DIR/${DOMAIN_NAME}.p
 
 docker run -d --name="haraka" \
     -p 127.0.0.1:25:25 \
+    -h $DOMAIN_NAME
+    -e DOMAIN_NAME=$DOMAIN_NAME \
     -v $HARAKA_DIR:/app/data girish/haraka:0.1
 
 chown -R yellowtent.yellowtent $HARAKA_DIR
