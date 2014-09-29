94e165242291b37882a0e26df0da3e42ecbe6c09
Johannes Zellner <webmaster@nebulon.de>
2014-09-28 20:09:56 -0700

    webadmin: add some basic field validation for user creation

--- webadmin/views/usercreate.js
+++ webadmin/views/usercreate.js
@@ -36,7 +36,10 @@ function UserCreateController ($scope, $routeParams, Client) {
 
         $scope.disabled = true;
         Client.createUser($scope.username, $scope.password, $scope.email, function (error) {
-            if (error && error.statusCode === 409) return console.error('Username already exists');
+            if (error && error.statusCode === 409) {
+                $scope.error.username = 'Username already taken';
+                return console.error('Username already taken');
+            }
             if (error) console.error('Unable to create user.', error);
 
             window.location.href = '#/userlist';
