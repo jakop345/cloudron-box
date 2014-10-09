f388cc6058d6ff0b0e8035f124fe3653c4143363
Girish Ramakrishnan <girish@forwardbias.in>
2014-10-08 23:42:32 -0700

    Stop container before deleting it
    This is attempting to fix
    Thu, 09 Oct 2014 06:30:22 GMT box:apptask Apptask completed for 326e4abd-aa72-4069-abf3-238757b3167d { [Error: HTTP code is 500 which indicates error: server error - Cannot destroy container 4524791d0efe279ce556a6c10c484bd09e5163f4279a5de84fa30836be49ebf4: Unable to remove filesystem for 4524791d0efe279ce556a6c10c484bd09e5163f4279a5de84fa30836be49ebf4: remove /var/lib/docker/containers/4524791d0efe279ce556a6c10c484bd09e5163f4279a5de84fa30836be49ebf4: directory not empty
    
    See https://github.com/docker/docker/issues/8203

--- src/apptask.js
+++ src/apptask.js
@@ -701,6 +701,9 @@ function uninstall(app, callback) {
         updateApp.bind(null, app, { installationProgress: 'Unconfiguring Nginx' }),
         unconfigureNginx.bind(null, app),
 
+        updateApp.bind(null, app, { installationProgress: 'Stopping app' }),
+        stopApp.bind(null, app),
+
         updateApp.bind(null, app, { installationProgress: 'Deleting container' }),
         deleteContainer.bind(null, app),
 
