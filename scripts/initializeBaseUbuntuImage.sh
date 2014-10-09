112e77dc207570329ec5900c76fea15eef5d3558
Girish Ramakrishnan <girish@forwardbias.in>
2014-10-08 19:35:35 -0700

    Log dropped incoming packages

--- scripts/initializeBaseUbuntuImage.sh
+++ scripts/initializeBaseUbuntuImage.sh
@@ -162,6 +162,12 @@ iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 iptables -A INPUT -p udp --sport 53 -j ACCEPT
 
+# log dropped incoming
+iptables -N LOGGING # new chain
+iptables -A INPUT -j LOGGING # last rule in INPUT chain
+iptables -A LOGGING -m limit --limit 2/min -j LOG --log-prefix "IPTables Packet Dropped: " --log-level 7
+iptables -A LOGGING -j DROP
+
 # loopback
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
