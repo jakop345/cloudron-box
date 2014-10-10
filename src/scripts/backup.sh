b0c2fd7129c551afcf6e99bf4cc2dcf7d749910b
Girish Ramakrishnan <girish@forwardbias.in>
2014-10-10 08:41:47 -0700

    Quote all the things!

--- src/scripts/backup.sh
+++ src/scripts/backup.sh
@@ -13,7 +13,7 @@ fi
 set -e
 
 NOW=$(date +%Y-%m-%dT%H:%M:%S)
-LOG=/var/log/cloudron/backup-${NOW}.log
+LOG="/var/log/cloudron/backup-${NOW}.log"
 exec 2>&1 1> $LOG
 
 if [ $# -lt 1 ]; then
@@ -31,24 +31,28 @@ CONTAINER_IDS=$(docker ps -q)
 
 # Stop all containers
 echo "Stopping all running containers"
-docker stop $CONTAINER_IDS
+if [ ! -z "$CONTAINER_IDS" ]; then
+    docker stop $CONTAINER_IDS
+fi
 
-FILE="backup_${NOW}.tar.gz"
+FILE="/tmp/backup_${NOW}.tar.gz"
 
-cd $HOME && tar czf /tmp/$FILE box .yellowtent
+cd "$HOME" && tar czf "$FILE" box .yellowtent
 echo "Uploading backup to $BACKUP_URL"
-curl -X PUT -T "/tmp/${FILE}" \
+curl -X PUT -T "$FILE" \
     "$BACKUP_URL"
 
-rm "/tmp/${FILE}"
+rm "$FILE"
 
 echo "Starting all containers"
 MAIL_SERVER="172.17.120.120"
-arp -d $MAIL_SERVER || echo "ARP does not have mail server entry"
-docker start $CONTAINER_IDS
+arp -d "$MAIL_SERVER" || echo "ARP does not have mail server entry"
+if [ ! -z "$CONTAINER_IDS" ]; then
+    docker start $CONTAINER_IDS
+fi
 
 # skip restarting the box code if a 2nd argument is provided, this is for the update case
-if [[ ! $2 ]]; then
+if [[ ! "$2" ]]; then
     echo "Starting box"
     supervisorctl start box
 fi
