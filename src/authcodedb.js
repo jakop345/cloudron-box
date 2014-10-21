84965de92c5b7e3e441bfe2e52f6352ff9d04058
Girish Ramakrishnan <girish@forwardbias.in>
2014-10-21 16:48:58 -0700

    fix typo

--- src/authcodedb.js
+++ src/authcodedb.js
@@ -59,7 +59,7 @@ function del(authCode, callback) {
 function clear(callback) {
     assert(typeof callback === 'function');
 
-    database.run('DELETE FROM authocodes', function (error) {
+    database.run('DELETE FROM authcodes', function (error) {
         if (error) return callback(new DatabaseError(DatabaseError.INTERNAL_ERROR, error));
 
         callback(null);
