689c1cc4c37ef41b4130a926bce3b5a442343b03
Girish Ramakrishnan <girish@forwardbias.in>
2014-10-22 00:35:01 -0700

    Actually add paths.js

---
+++ src/paths.js
@@ -0,0 +1,24 @@
+/* jslint node:true */
+
+'use strict';
+
+var config = require('../config.js'),
+    path = require('path');
+
+exports = module.exports = {
+    NGINX_CONFIG_DIR: path.join(config.baseDir, 'nginx'),
+    NGINX_APPCONFIG_DIR: path.join(config.baseDir, 'nginx/applications'),
+    NGINX_CERT_DIR: path.join(config.baseDir, 'nginx/cert'),
+
+    HARAKA_CONFIG_DIR: path.join(config.baseDir, 'haraka'),
+
+    COLLECTD_APPCONFIG_DIR: path.join(config.baseDir, 'collectd/collectd.conf.d'),
+
+    APPDATA_DIR: path.join(config.baseDir, 'data/appdata'),
+
+    APPICONS_DIR: path.join(config.baseDir, 'appicons'),
+
+    VOLUMES_DATA_DIR: path.join(config.baseDir, 'volumes/data'),
+    VOLUMES_MOUNT_DIR: path.join(config.baseDir, 'volumes/mount')
+};
+
