93d3245b42f49e0d646fdbf968d8cfe12098f66a
Girish Ramakrishnan <girish@forwardbias.in>
2014-10-21 23:42:01 -0700

    Clarify cloudron.conf vs cloudron.sqlite

--- config.js
+++ config.js
@@ -21,6 +21,8 @@ if (production) {
     config.baseDir = path.join(homeDir, '.yellowtenttest');
 }
 
+// cloudron.conf contains 'instance' config that applies to this cloudron instance. This doesn't need
+// to be backed up. cloudron.sqlite contains all data that will be backed up
 config.cloudronConfigFile = path.join(config.baseDir, 'cloudron.conf');
 config.databaseFileName = path.join(config.baseDir, 'data/cloudron.sqlite');
 
