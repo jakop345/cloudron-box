9b496ff3ac1f5ed795bf5e4d38e44e3f07b40045
Girish Ramakrishnan <girish@forwardbias.in>
2014-11-03 13:32:53 -0800

    Add separate versions file for dev and stable

--- release/publish.js
+++ release/publish.js
@@ -10,54 +10,64 @@ function die(msg) {
     process.exit(1);
 }
 
-// check if the json is valid
-var versionsJson = safe.JSON.parse(fs.readFileSync(path.join(__dirname, 'versions.json')));
-if (!versionsJson) {
-    die('versions.json is not valid json : ' + safe.error);
+function publish(versionsFileName) {
+    // check if the json is valid
+    var versionsJson = safe.JSON.parse(fs.readFileSync(versionsFileName));
+    if (!versionsJson) {
+        die(versionsFileName + ' is not valid json : ' + safe.error);
+    }
+
+    // check all the keys
+    var sortedVersions = Object.keys(versionsJson).sort();
+    sortedVersions.forEach(function (version, index) {
+        if (typeof versionsJson[version].imageId !== 'string') die('version ' + version + ' does not have proper imageId');
+        if (versionsJson[version].next !== null && typeof versionsJson[version].next !== 'string') die('version ' + version + ' does not have proper next');
+        if (typeof versionsJson[version].revision !== 'string') die('version ' + version + ' does not have proper revision');
+
+        var nextVersion = versionsJson[version].next;
+        if (nextVersion <= version) die('next version cannot be less than current @' + version);
+    });
+
+    // check that package.json version is in versions.json
+    var currentVersion = require('../package.json').version;
+    if (sortedVersions.indexOf(currentVersion) === -1) {
+        die('package.json version is not present in versions.json');
+    }
+
+    if (sortedVersions.indexOf(currentVersion) !== sortedVersions.length - 1) {
+        die('package.json version is not the latest version in ' + versionsFileName);
+    }
+
+    var config = {
+        accessKeyId: 'AKIAJ3GNZ2C7W5XKAH7Q',
+        secretAccessKey: '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'
+    };
+
+    var s3 = new AWS.S3(config);
+
+    var versionsFileStream = fs.createReadStream(versionsFileName);
+
+    var params = {
+        Bucket: 'cloudron-releases',
+        Key: path.basename(versionsFileName),
+        ACL: 'public-read',
+        Body: versionsFileStream,
+        ContentType: 'application/json'
+    };
+
+    console.log('Uploading ' + path.basename(versionsFileName));
+    s3.putObject(params, function (error, data) {
+        if (error) return console.error(error);
+
+        console.log(data);
+    });
 }
 
-// check all the keys
-var sortedVersions = Object.keys(versionsJson).sort();
-sortedVersions.forEach(function (version, index) {
-    if (typeof versionsJson[version].imageId !== 'string') die('version ' + version + ' does not have proper imageId');
-    if (versionsJson[version].next !== null && typeof versionsJson[version].next !== 'string') die('version ' + version + ' does not have proper next');
-    if (typeof versionsJson[version].revision !== 'string') die('version ' + version + ' does not have proper revision');
-
-    var nextVersion = versionsJson[version].next;
-    if (nextVersion <= version) die('next version cannot be less than current @' + version);
-});
-
-// check that package.json version is in versions.json
-var currentVersion = require('../package.json').version;
-if (sortedVersions.indexOf(currentVersion) === -1) {
-    die('package.json version is not present in versions.json');
+if (process.argv.length === 3) {
+    publish(path.join(__dirname, 'versions-' + process.argv[2] + '.json'));
+} else {
+    console.log('publish.sh <dev|stable>');
 }
 
-if (sortedVersions.indexOf(currentVersion) !== sortedVersions.length - 1) {
-    die('package.json version is not the latest version in versions.json');
-}
 
-var config = {
-    accessKeyId: 'AKIAJ3GNZ2C7W5XKAH7Q',
-    secretAccessKey: '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'
-};
-
-var s3 = new AWS.S3(config);
-
-var versionsFileStream = fs.createReadStream(path.join(__dirname, 'versions.json'));
-
-var params = {
-    Bucket: 'cloudron-releases',
-    Key: 'versions.json',
-    ACL: 'public-read',
-    Body: versionsFileStream,
-    ContentType: 'application/json'
-};
-
-console.log('Uploading versions.json');
-s3.putObject(params, function (error, data) {
-    if (error) return console.error(error);
-
-    console.log(data);
-});
 
