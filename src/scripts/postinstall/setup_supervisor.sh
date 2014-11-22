9b4965b22fafd5239f2d583eb5191c0e6be46e05
Girish Ramakrishnan <girish@forwardbias.in>
2014-11-21 22:32:37 -0800

    Set logfile name explicitly
    From the docs:
    AUTO log files and their backups will be deleted when supervisord restarts.
    
    This has the result that log files disappear after every update

--- src/scripts/postinstall/setup_supervisor.sh
+++ src/scripts/postinstall/setup_supervisor.sh
@@ -14,36 +14,41 @@ mkdir -p /etc/supervisor
 mkdir -p /etc/supervisor/conf.d
 cp $SRCDIR/supervisor/supervisord.conf /etc/supervisor/
 
-echo "Writing nginx supervisor config..."
+echo "Writing supervisor configs..."
+
 cat > /etc/supervisor/conf.d/nginx.conf <<EOF
 [program:nginx]
 command=/usr/sbin/nginx -c "$NGINX_CONFIG_DIR/nginx.conf" -p /var/log/nginx/
 autostart=true
 autorestart=true
 redirect_stderr=true
+stdout_logfile=nginx.log
+stdout_logfile_maxbytes=50MB
+stdout_logfile_backups=2
 EOF
-echo "Done"
 
-echo "Writing box supervisor config..."
 cat > /etc/supervisor/conf.d/box.conf <<EOF
 [program:box]
-command=/usr/bin/node app.js
+command=/usr/bin/node $SRCDIR/app.js
 autostart=true
 autorestart=true
 redirect_stderr=true
-directory=$SRCDIR
+stdout_logfile=box.log
+stdout_logfile_maxbytes=50MB
+stdout_logfile_backups=2
 user=yellowtent
 environment=HOME="/home/yellowtent",CLOUDRON="1",USER="yellowtent",DEBUG="box*"
 EOF
 
-echo "Writing updater supervisor config..."
 cat > /etc/supervisor/conf.d/updater.conf <<EOF
 [program:updater]
-command=/usr/bin/node server.js update-mode
+command=/usr/bin/node $SRCDIR/installer/server.js update-mode
 autostart=true
 autorestart=true
 redirect_stderr=true
-directory=$SRCDIR/installer
+stdout_logfile=updater.log
+stdout_logfile_maxbytes=50MB
+stdout_logfile_backups=2
 user=yellowtent
 environment=HOME="/home/yellowtent",CLOUDRON="1",USER="yellowtent",DEBUG="installer*,connect-lastmile"
 EOF
