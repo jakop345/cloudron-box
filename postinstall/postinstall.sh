7c5433574639a17226cc0a7432defa4937048c0f
Girish Ramakrishnan <girish@forwardbias.in>
2014-11-24 19:17:48 -0800

    Add webadmin client correctly in the database

--- postinstall/postinstall.sh
+++ postinstall/postinstall.sh
@@ -147,7 +147,11 @@ sqlite3 "$CLOUDRON_SQLITE" 'UPDATE apps SET installationState = "pending_restore
 echo "Add webadmin oauth cient"
 ADMIN_SCOPES="root,profile,users,apps,settings,roleAdmin"
 ADMIN_ID=$(cat /proc/sys/kernel/random/uuid)
-sqlite3 "$CLOUDRON_SQLITE" 'INSERT OR REPLACE INTO clients (id, appId, clientId, clientSecret, name, redirectURI, scope) VALUES ("$ADMIN_ID", "webadmin", "cid-webadmin", "unusedsecret", "WebAdmin", "$ADMIN_ORIGIN", "$ADMIN_SCOPES")'
+QUERY=$(cat <<EOF
+    INSERT OR REPLACE INTO clients (id, appId, clientId, clientSecret, name, redirectURI, scope) VALUES ("$ADMIN_ID", "webadmin", "cid-webadmin", "unusedsecret", "WebAdmin", "$ADMIN_ORIGIN", "$ADMIN_SCOPES")
+EOF
+)
+sqlite3 "$CLOUDRON_SQLITE" "$QUERY"
 EOF
 
 echo "==== Setup supervisord ===="
