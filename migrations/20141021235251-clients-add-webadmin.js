b795684791ca148d9050c7bea5b2c07d128b3080
Girish Ramakrishnan <girish@forwardbias.in>
2014-11-24 19:24:04 -0800

    Only add webadmin client entry in test mode

--- migrations/20141021235251-clients-add-webadmin.js
+++ migrations/20141021235251-clients-add-webadmin.js
@@ -7,6 +7,9 @@ exports.up = function(db, callback) {
     var scopes = 'root,profile,users,apps,settings,roleAdmin';
     var adminOrigin = 'https://admin-localhost';
 
+    // postinstall.sh creates the webadmin entry in production mode
+    if (process.env.NODE_ENV !== 'test') return callback(null);
+
     db.runSql('INSERT INTO clients (id, appId, clientId, clientSecret, name, redirectURI, scope) ' +
               'VALUES (?, ?, ?, ?, ?, ?, ?)', [ uuid.v4(), 'webadmin', 'cid-webadmin', 'unused', 'WebAdmin', adminOrigin, scopes ],
               callback);
