d4a493ae741586a2f97e453bab7f4f46ffe1d1bc
Girish Ramakrishnan <girish@forwardbias.in>
2015-01-18 15:50:25 -0800

    Add optional versions file argument to new

--- release/publish.sh
+++ release/publish.sh
@@ -18,13 +18,14 @@ JSON="$SOURCE_DIR/node_modules/.bin/json"
 VERSIONS_URL="https://s3.amazonaws.com/cloudron-releases/versions-dev.json"
 VERSIONS_S3_URL="s3://cloudron-releases/versions-dev.json"
 
+NEW_VERSIONS_FILE=""
 SOURCE_TARBALL_URL=""
 IMAGE_ID=""
 CMD=""
 
 # --code and--image is provided for readability. The code below assumes number is an image id
 # and anything else is the source tarball url. So, one can just say "publish.sh 2345 https://foo.tar.gz"
-ARGS=$($GNU_GETOPT -o "" -l "dev,stable,code:,image:,rerelease,new,list" -n "$0" -- "$@")
+ARGS=$($GNU_GETOPT -o "" -l "dev,stable,code:,image:,rerelease,new:,list" -n "$0" -- "$@")
 eval set -- "$ARGS"
 
 while true; do
@@ -34,7 +35,7 @@ while true; do
     --code) SOURCE_TARBALL_URL="$2"; shift 2;;
     --image) IMAGE_ID="$2"; shift 2;;
     --rerelease) CMD="rerelease"; shift;;
-    --new) CMD="new"; shift;;
+    --new) CMD="new"; NEW_VERSIONS_FILE="$2"; shift 2;;
     --list) CMD="list"; shift;;
     --) shift; break;;
     *) echo "Unknown option $2"; exit;;
@@ -48,47 +49,50 @@ while test $# -gt 0; do
     shift
 done
 
-NEW_VERSIONS_FILE=$(mktemp -t box-versions 2>/dev/null || mktemp)
-cleanup() {
-    rm "$NEW_VERSIONS_FILE"
-}
-trap cleanup EXIT
+if [ -z "$NEW_VERSIONS_FILE" ]; then
+    NEW_VERSIONS_FILE=$(mktemp -t box-versions 2>/dev/null || mktemp)
+    cleanup() {
+        rm "$NEW_VERSIONS_FILE"
+    }
+    trap cleanup EXIT
+
+    if ! wget -q -O "$NEW_VERSIONS_FILE" "$VERSIONS_URL"; then
+        echo "Error downloading versions file"
+        exit 1
+    fi
+fi
 
 if [[ "$CMD" == "list" ]]; then
-    $SOURCE_DIR/node_modules/.bin/s3-cli get "$VERSIONS_S3_URL" "$NEW_VERSIONS_FILE"
     cat "$NEW_VERSIONS_FILE"
     exit 0
 fi
 
 if [[ "$CMD" == "new" ]]; then
-    # generate a new versions.json
-    if [[ -z "$SOURCE_TARBALL_URL" || -z "$IMAGE_ID" ]]; then
-        echo "--code and --image is required"
-        exit 1
-    fi
+    # generate a new versions.json if the user hasn't provided one
+    if [ -z "$NEW_VERSIONS_FILE" ]; then
+        if [[ -z "$SOURCE_TARBALL_URL" || -z "$IMAGE_ID" ]]; then
+            echo "--code and --image is required"
+            exit 1
+        fi
 
-    NEW_VERSION="0.0.1"
-    IMAGE_NAME=$(get_image_name $IMAGE_ID)
+        NEW_VERSION="0.0.1"
+        IMAGE_NAME=$(get_image_name $IMAGE_ID)
 
-    cat > "$NEW_VERSIONS_FILE" <<EOF
-    {
-        "0.0.1": {
-            "sourceTarballUrl": "$SOURCE_TARBALL_URL",
-            "imageId": $IMAGE_ID,
-            "imageName": "$IMAGE_NAME",
-            "next": null
+        cat > "$NEW_VERSIONS_FILE" <<EOF
+        {
+            "0.0.1": {
+                "sourceTarballUrl": "$SOURCE_TARBALL_URL",
+                "imageId": $IMAGE_ID,
+                "imageName": "$IMAGE_NAME",
+                "next": null
+            }
         }
-    }
 EOF
+    fi
 else
     # modify existing versions.json
     if [[ -z "$SOURCE_TARBALL_URL" && -z "$IMAGE_ID" && "$CMD" != "rerelease" ]]; then
         echo "--code or --image is required"
-        exit 1
-    fi
-
-    if ! wget -q -O "$NEW_VERSIONS_FILE" "$VERSIONS_URL"; then
-        echo "Error downloading versions file"
         exit 1
     fi
 
@@ -103,13 +107,12 @@ else
     fi
 
     NEW_VERSION=$($SOURCE_DIR/node_modules/.bin/semver -i $LAST_VERSION)
+    echo "Releasing version $NEW_VERSION"
     IMAGE_NAME=$(get_image_name $IMAGE_ID)
 
     $JSON -q -I -f "$NEW_VERSIONS_FILE" -e "this['$LAST_VERSION'].next = '$NEW_VERSION'"
     $JSON -q -I -f "$NEW_VERSIONS_FILE" -e "this['$NEW_VERSION'] = { 'sourceTarballUrl': '$SOURCE_TARBALL_URL', 'imageId': $IMAGE_ID, 'imageName': '$IMAGE_NAME', 'next': null }"
 fi
-
-echo "Releasing version $NEW_VERSION"
 
 echo "Verifying new versions file"
 $SOURCE_DIR/release/verify.js "$NEW_VERSIONS_FILE"
