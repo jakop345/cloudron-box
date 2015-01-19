bb600a1896f234661f35976768a475b33f365d8f
Johannes Zellner <johannes@nebulon.de>
2015-01-19 19:25:40 +0100

    Use regular OAuth flow for proxy auth

--- proxy.js
+++ proxy.js
@@ -9,8 +9,11 @@ var express = require('express'),
     http = require('http');
 
 var gSessions = {};
+var gProxyMiddlewareCache = {};
 var gApp = express();
 var gHttpServer = http.createServer(gApp);
+
+var CALLBACK_URI = '/callback';
 
 gHttpServer.on('error', console.error);
 
@@ -21,25 +24,50 @@ gApp.use(session({
 gApp.use(function (req, res, next) {
     if (req.session && gSessions[req.session.sessid]) return next();
 
-    if (req.path === '/login_callback.html') {
+    if (req.path === CALLBACK_URI) {
         // FIXME we need to exchange the authCode and verify it
         req.session.sessid = req.query.authCode;
+
+        // this is a simple in memory auth store
         gSessions[req.session.sessid] = 'ok';
 
-        console.log('FIXME: this is not secure, only the authCode: %s.', req.query.authCode);
-
-        next();
+        // now redirect to the actual initially requested URL
+        res.redirect(req.session.returnTo);
     } else {
-        var callbackURL = 'http://localhost:4000/login_callback.html';
+        var forwardedHost = req.headers['x-forwarded-host'];
+        var forwardedProto = req.headers['x-forwarded-proto'];
+        var port = parseInt(req.headers['x-cloudron-proxy-port'], 10);
+
+        if (!forwardedProto) return res.send(500, 'Routing error. No forwarded protocol.');
+        if (!forwardedHost) return res.send(500, 'Routing error. No forwarded host.');
+        if (!Number.isFinite(port)) return res.send(500, 'Routing error. No forwarded port.');
+
+        req.session.port = port;
+        req.session.returnTo =  forwardedProto + '://' + forwardedHost + req.path;
+
+        var callbackURL = forwardedProto + '://' + forwardedHost + CALLBACK_URI;
         var scope = 'root,profile,apps,roleAdmin';
         var clientId = 'cid-proxy';
         var oauthLogin = 'https://admin-localhost/api/v1/oauth/dialog/authorize?response_type=code&client_id=' + clientId + '&redirect_uri=' + callbackURL + '&scope=' + scope;
 
+        // begin the OAuth flow
         res.redirect(oauthLogin);
     }
 });
 
-gApp.use(proxy(url.parse('http://localhost:8000')));
+gApp.use(function (req, res, next) {
+    var port = req.session.port;
+
+    var proxyMiddleware = gProxyMiddlewareCache[port];
+    if (!proxyMiddleware) {
+        console.log('Adding proxy middleware for port %d', port);
+
+        proxyMiddleware = proxy(url.parse('http://127.0.0.1:' + port));
+        gProxyMiddlewareCache[port] = proxyMiddleware;
+    }
+
+    proxyMiddleware(req, res, next);
+});
 
 gHttpServer.listen(4000, function () {
     console.log('Proxy server listening...');
