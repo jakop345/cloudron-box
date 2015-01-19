4987810ee613b46bef52efbfadfcd7f7a058a88a
Johannes Zellner <johannes@nebulon.de>
2015-01-19 23:27:44 +0100

    Add proxy supervisor config

--- postinstall/setup_supervisord.sh
+++ postinstall/setup_supervisord.sh
@@ -26,3 +26,16 @@ stdout_logfile_backups=2
 user=yellowtent
 environment=HOME="/home/yellowtent",CLOUDRON="1",USER="yellowtent",DEBUG="box*,connect-lastmile",NODE_ENV="production"
 EOF
+
+cat > /etc/supervisor/conf.d/proxy.conf <<EOF
+[program:proxy]
+command=/usr/bin/node $BOX_SRCDIR/proxy.js
+autostart=true
+autorestart=true
+redirect_stderr=true
+stdout_logfile=/var/log/supervisor/proxy.log
+stdout_logfile_maxbytes=50MB
+stdout_logfile_backups=2
+user=yellowtent
+environment=HOME="/home/yellowtent",CLOUDRON="1",USER="yellowtent",DEBUG="box*",NODE_ENV="production"
+EOF
