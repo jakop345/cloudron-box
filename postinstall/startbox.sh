452c0cce8a7e172caa57cef90fff285e626a077e
Girish Ramakrishnan <girish@forwardbias.in>
2015-01-20 00:43:50 -0800

    Reload supervisor in startbox

--- postinstall/startbox.sh
+++ postinstall/startbox.sh
@@ -2,11 +2,25 @@
 
 set -e
 
-echo "Starting box code"
+# looks like restarting supervisor completely is the only way to reload it
+reload_supervisor() {
+    service supervisor stop || true
 
-service supervisor start
+    echo -n "Waiting for supervisord to stop"
+    while test -e "/var/run/supervisord.pid" && kill -0 `cat /var/run/supervisord.pid`; do
+        echo -n "."
+        sleep 1
+    done
+    echo ""
 
-sleep 2 # give supervisor sometime to start the processes
+    echo "Starting supervisor"
+
+    service supervisor start
+
+    sleep 2 # give supervisor sometime to start the processes
+}
+
+reload_supervisor
 
 nginx -s reload
 
