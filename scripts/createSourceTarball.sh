d149519fc1bd9d2b85b6110c0aab614056f6413b
Girish Ramakrishnan <girish@forwardbias.in>
2015-01-21 14:29:45 -0800

    createSourceTarball: fix coding style

--- scripts/createSourceTarball.sh
+++ scripts/createSourceTarball.sh
@@ -3,28 +3,27 @@
 # set -x
 set -e
 
-[ ! -f "$HOME/.s3cfg" ] && echo "~/.s3cfg missing" && exit 1
+[[ ! -f "${HOME}/.s3cfg" ]] && echo "~/.s3cfg missing" && exit 1
 
-SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd )"
-VERSION=$(cd "$SOURCE_DIR" && git rev-parse HEAD)
+readonly SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
+readonly TMPDIR=${TMPDIR:-/tmp} # why is this not set on mint?
 
-# why is this not set on mint?
-TMPDIR=${TMPDIR:-/tmp}
+readonly version=$(cd "${SOURCE_DIR}" && git rev-parse HEAD)
+readonly bundle_dir=$(mktemp -d -t box 2>/dev/null || mktemp -d box-XXXXXXXXXX --tmpdir=$TMPDIR)
+readonly bundle_file="${TMPDIR}/box-${version}.tar.gz"
 
-BUNDLE_DIR=$(mktemp -d -t box 2>/dev/null || mktemp -d box-XXXXXXXXXX --tmpdir=$TMPDIR)
-chmod "o+rx,g+rx" "$BUNDLE_DIR" # otherwise extracted tarball director won't be readable by others/group
-echo "Checking out code [$VERSION] into $BUNDLE_DIR"
-(cd "$SOURCE_DIR" && git archive --format=tar HEAD | (cd "$BUNDLE_DIR" && tar xf -))
+chmod "o+rx,g+rx" "${bundle_dir}" # otherwise extracted tarball director won't be readable by others/group
+echo "Checking out code [${version}] into ${bundle_dir}"
+(cd "${SOURCE_DIR}" && git archive --format=tar HEAD | (cd "${bundle_dir}" && tar xf -))
 
 echo "Installing modules"
-cd "$BUNDLE_DIR" && npm install --production
+cd "${bundle_dir}" && npm install --production
 
-BUNDLE_FILE="$TMPDIR/box-${VERSION}.tar.gz"
-cd "$BUNDLE_DIR" && tar czvf "$BUNDLE_FILE" .
+cd "${bundle_dir}" && tar czvf "${bundle_file}" .
 
 echo "Uploading bundle to S3"
-$SOURCE_DIR/node_modules/.bin/s3-cli put --acl-public "$BUNDLE_FILE" "s3://cloudron-releases/box-${VERSION}.tar.gz"
+${SOURCE_DIR}/node_modules/.bin/s3-cli put --acl-public "${bundle_file}" "s3://cloudron-releases/box-${version}.tar.gz"
 
-echo "Cleaning up $BUNDLE_DIR"
-rm -rf "$BUNDLE_DIR" "$BUNDLE_FILE"
+echo "Cleaning up ${bundle_dir}"
+rm -rf "${bundle_dir}" "${bundle_file}"
 
