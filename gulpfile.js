99283967e9714d939b2cef0178a03914a7465dde
Johannes Zellner <johannes@nebulon.de>
2015-01-21 17:07:03 +0100

    Add gulp to allow templating of static webadmin content
    Currently only webadmin/error.ejs is templated. gulp html
    will generated a error.html based on the values in
    deploymentConfig.json
    
    Currently we also commit the templating output to the relevant
    branch (eg. for development branch master). This means if a
    templated file was changed, prior to committing a change we have
    to run gulp to generate the output.
    
    Fixes #132

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+/* jslint node:true */
+
+'use strict';
+
+var _ejs = require('ejs'),
+    ejs = require('gulp-ejs'),
+    gulp = require('gulp'),
+    path = require('path'),
+    fs = require('fs');
+
+_ejs.filters.basename = function (obj) {
+    return path.basename(obj);
+};
+
+gulp.task('html', function () {
+    var config = JSON.parse(fs.readFileSync('./deploymentConfig.json'));
+
+    gulp.src('webadmin/*.ejs')
+        .pipe(ejs(config, { ext: '.html' }))
+        .pipe(gulp.dest('webadmin'));
+});
+
+gulp.task('default', function () {
+    gulp.start('html');
+});
