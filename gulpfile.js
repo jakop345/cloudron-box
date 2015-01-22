476f65b24efa182b849acd5afeb78e0566fedff8
Johannes Zellner <johannes@nebulon.de>
2015-01-22 09:11:08 +0100

    webadmin: Use gulp to process html files

--- gulpfile.js
+++ gulpfile.js
@@ -12,12 +12,17 @@ _ejs.filters.basename = function (obj) {
     return path.basename(obj);
 };
 
-gulp.task('html', function () {
+gulp.task('html', ['html_templates'], function () {
+    gulp.src('webadmin/**/*.html')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html_templates', function () {
     var config = JSON.parse(fs.readFileSync('./deploymentConfig.json'));
 
     gulp.src('webadmin/*.ejs')
         .pipe(ejs(config, { ext: '.html' }))
-        .pipe(gulp.dest('webadmin'));
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', function () {
