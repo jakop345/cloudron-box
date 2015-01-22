357617997e4eab3bb90a264cba233261a7c61600
Girish Ramakrishnan <girish@forwardbias.in>
2015-01-22 00:16:29 -0800

    Configure nginx to show update page when stopping box
    This code was moved from the installer code

--- setup/stopbox.sh
+++ setup/stopbox.sh
@@ -2,6 +2,36 @@
 
 set -e
 
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly NGINX_CONFIG_DIR=/home/yellowtent/setup_configs/nginx # do not reuse configs since it will be removed by installer
+
+readonly CERT_DIR="/home/yellowtent/configs/nginx/cert"
+
+setup_nginx() {
+    if [[ ! -f "${CERT_DIR}/host.key" || ! -f "${CERT_DIR}/host.key" ]]; then
+        echo "Skipping settings up nginx since no certs found"
+        return
+    fi
+
+    echo "Setting up nginx update page"
+
+    # show updating page in nginx
+    local provision_tls_cert=$(cat "${CERT_DIR}/host.cert")
+    local provision_tls_key=$(cat "${CERT_DIR}/host.key")
+
+    unlink /etc/nginx 2>/dev/null || rm -rf /etc/nginx
+    rm -rf "${NGINX_CONFIG_DIR}" && mkdir -p "${NGINX_CONFIG_DIR}"
+    ln -s "${NGINX_CONFIG_DIR}" /etc/nginx
+    cp "${SCRIPT_DIR}/nginx/nginx.conf" "${NGINX_CONFIG_DIR}/nginx.conf"
+    cp "${SCRIPT_DIR}/nginx/mime.types" "${NGINX_CONFIG_DIR}/mime.types"
+    mkdir -p "${NGINX_CONFIG_DIR}/cert"
+    echo "${provision_tls_cert}" > "${NGINX_CONFIG_DIR}/cert/host.cert"
+    echo "${provision_tls_key}" > "${NGINX_CONFIG_DIR}/cert/host.key"
+    nginx -s reload
+}
+
+setup_nginx
+
 echo "Stopping box code"
 
 service supervisor stop || true
