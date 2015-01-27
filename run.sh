5a84681ef391e20c1515921bcec4ae9ebf8cac65
Johannes Zellner <johannes@nebulon.de>
2015-01-27 14:38:44 +0100

    Adjust run.sh to accomodate postinstall -> start rename

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ readonly NGINX_ROOT=~/.yellowtent/nginx
 readonly PROVISION_VERSION=0.1
 readonly PROVISION_BOX_VERSIONS_URL=0.1
 readonly DATA_DIR=~/.yellowtent/data
+readonly FQDN=admin-localhost
 
 mkdir -p "${NGINX_ROOT}/applications"
 mkdir -p "${NGINX_ROOT}/cert"
@@ -17,20 +18,19 @@ mkdir -p "${DATA_DIR}"
 # get the database current
 npm run-script migrate
 
-cp setup/postinstall/nginx/nginx.conf "${NGINX_ROOT}/nginx.conf"
-cp setup/postinstall/nginx/mime.types "${NGINX_ROOT}/mime.types"
-cp setup/postinstall/nginx/cert/* "${NGINX_ROOT}/cert/"
+cp setup/start/nginx/nginx.conf "${NGINX_ROOT}/nginx.conf"
+cp setup/start/nginx/mime.types "${NGINX_ROOT}/mime.types"
+cp setup/start/nginx/cert/* "${NGINX_ROOT}/cert/"
 
 # adjust the generated nginx config for local use
 touch "${NGINX_ROOT}/naked_domain.conf"
-sed -e "s/##ADMIN_FQDN##/admin-localhost/" -e "s|##BOX_SRC_DIR##|${BOX_SRC_DIR}|" setup/postinstall/nginx/admin.conf_template > "${NGINX_ROOT}/applications/admin.conf"
+sed -e "s/##ADMIN_FQDN##/${FQDN}/" -e "s|##BOX_SRC_DIR##|${BOX_SRC_DIR}|" setup/start/nginx/admin.conf_template > "${NGINX_ROOT}/applications/admin.conf"
 sed -e "s/user www-data/user ${USER}/" -i "${NGINX_ROOT}/nginx.conf"
 
 # add webadmin oauth client
 readonly WEBADMIN_ID=abcdefg
-readonly WEBADMIN_ORIGIN=https://admin-localhost
 readonly WEBADMIN_SCOPES="root,profile,users,apps,settings,roleAdmin"
-sqlite3 "${DATA_DIR}/cloudron.sqlite" "INSERT OR REPLACE INTO clients (id, appId, clientId, clientSecret, name, redirectURI, scope) VALUES (\"${WEBADMIN_ID}\", \"webadmin\", \"cid-webadmin\", \"secret-webadmin\", \"WebAdmin\", \"${WEBADMIN_ORIGIN}\", \"${WEBADMIN_SCOPES}\")"
+sqlite3 "${DATA_DIR}/cloudron.sqlite" "INSERT OR REPLACE INTO clients (id, appId, clientId, clientSecret, name, redirectURI, scope) VALUES (\"${WEBADMIN_ID}\", \"webadmin\", \"cid-webadmin\", \"secret-webadmin\", \"WebAdmin\", \"https://${FQDN}\", \"${WEBADMIN_SCOPES}\")"
 
 # start nginx
 sudo nginx -c nginx.conf -p "${NGINX_ROOT}"
