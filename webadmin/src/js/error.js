28a9c0eee94556447188e22604faed938256a421
Johannes Zellner <johannes@nebulon.de>
2015-02-24 16:57:11 +0100

    Allow to contextualize the error.html

--- webadmin/src/js/error.js
+++ webadmin/src/js/error.js
@@ -5,11 +5,19 @@ var app = angular.module('Application', []);
 
 app.controller('ErrorController', ['$scope', '$http', function ($scope, $http) {
     $scope.webServerOriginLink = null;
+    $scope.errorMessage = '';
 
+    // try to fetch at least config.json to get appstore url
     $http.get('config.json').success(function(data, status) {
         if (status !== 200 || typeof data !== 'object') return console.error(status, data);
         $scope.webServerOrigin = data.webServerOrigin + '/console.html';
     }).error(function (data, status) {
-        console.error(status, data);
+        if (status === 404) console.error('No config.json found');
+        else console.error(status, data);
     });
+
+    var search = window.location.search.slice(1).split('&').map(function (item) { return item.split('='); }).reduce(function (o, k) { o[k[0]] = k[1]; return o; }, {});
+
+    $scope.errorCode = search.errorCode || 0;
+    $scope.errorContext = search.errorContext || '';
 }]);
--- webadmin/src/js/main.js
+++ webadmin/src/js/main.js
@@ -23,7 +23,7 @@ angular.module('Application').controller('MainController', ['$scope', '$route', 
     };
 
     $scope.setup = function () {
-        window.location.href = '/error.html';
+        window.location.href = '/error.html?errorCode=1';
     };
 
     $scope.error = function (error) {
