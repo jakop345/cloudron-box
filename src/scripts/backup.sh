fbb46c51c8ac5849302eed4c803e81bfb3793a0c
Girish Ramakrishnan <girish@forwardbias.in>
2015-03-09 14:57:23 -0700

    Try upto 5 times to upload backup

--- src/scripts/backup.sh
+++ src/scripts/backup.sh
@@ -30,8 +30,16 @@ mysqldump -u root -ppassword box > "${DATA_DIR}/box.mysqldump"
 echo "Snapshoting backup as backup-${now}"
 btrfs subvolume snapshot -r "${DATA_DIR}" "${HOME}/backup-${now}"
 
-echo "Uploading backup to ${backup_url}"
-tar -cvzf - -C "${HOME}/backup-${now}" . | openssl aes-256-cbc -e -pass "pass:${backup_key}" | curl --fail -H "Content-Type:" -X PUT --data-binary @- "${backup_url}"
+for i in `seq 1 5`; do
+    echo "Uploading backup to ${backup_url} (try ${i})"
+    if tar -cvzf - -C "${HOME}/backup-${now}" . \
+           | openssl aes-256-cbc -e -pass "pass:${backup_key}" \
+           | curl --fail -H "Content-Type:" -X PUT --data-binary @- "${backup_url}" 2>/tmp/backup_upload.log; then
+        break
+    fi
+    cat /tmp/backup_upload.log && rm /tmp/backup_upload.log
+    [[ $i -eq 5 ]] && exit 1
+done
 
 echo "Deleting backup snapshot"
 btrfs subvolume delete "${HOME}/backup-${now}"
