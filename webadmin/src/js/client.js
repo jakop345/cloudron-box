6cd53700b40fdb0a03d46a20dc949cc00ca42daa
Johannes Zellner <johannes@nebulon.de>
2015-03-27 13:11:51 +0100

    Add Client.progress() to fetch the current update/backup progress

--- webadmin/src/js/client.js
+++ webadmin/src/js/client.js
@@ -232,6 +232,13 @@ angular.module('Application').service('Client', ['$http', 'md5', 'Notification',
         }).error(defaultErrorHandler(callback));
     };
 
+    Client.prototype.progress = function (callback) {
+        $http.get('/api/v1/cloudron/progress').success(function(data, status) {
+            if (status !== 200 || typeof data !== 'object') return callback(new ClientError(status, data));
+            callback(null, data);
+        }).error(defaultErrorHandler(callback));
+    };
+
     Client.prototype.version = function (callback) {
         $http.get('/api/v1/cloudron/status').success(function(data, status) {
             if (status !== 200 || typeof data !== 'object') return callback(new ClientError(status, data));
