d2bdfc3e48eb4940b0db7db7938762e1d6a42745
Girish Ramakrishnan <girish@forwardbias.in>
2015-03-29 19:01:40 -0700

    Upload snapshot of apps data
    btrfs snapshotting does not really give us much in terms of atomicity
    since the addons are dumped non-atomically.
    
    The only way to make it all atomic is to stop the app. Snapshot and
    let the app start asap. Because we don't stop apps when taking a backup (atm),
    the whole btrfs snapshotting thing is superfluous.
    
    Part of #280

--- src/scripts/backupapp.sh
+++ src/scripts/backupapp.sh
@@ -24,19 +24,24 @@ readonly DATA_DIR="${HOME}/data"
 app_id="$1"
 backup_url="$2"
 backup_key="$3"
-now=$(date "+%Y-%m-%dT%H:%M:%S")
-app_data_dir="${DATA_DIR}/${app_id}"
+readonly now=$(date "+%Y-%m-%dT%H:%M:%S")
+readonly app_data_dir="${DATA_DIR}/${app_id}"
+readonly app_data_snapshot="${DATA_DIR}/${app_id}-${now}"
+
+btrfs subvolume snapshot -r "${app_data_dir}" "${app_data_snapshot}"
 
 for try in `seq 1 5`; do
     echo "Uploading backup to ${backup_url} (try ${try})"
     error_log=$(mktemp)
-    if tar -cvzf - -C "${app_data_dir}" . \
+    if tar -cvzf - -C "${app_data_snapshot}" . \
            | openssl aes-256-cbc -e -pass "pass:${backup_key}" \
            | curl --fail -H "Content-Type:" -X PUT --data-binary @- "${backup_url}" 2>"${error_log}"; then
         break
     fi
     cat "${error_log}" && rm "${error_log}"
 done
+
+btrfs subvolume delete "${app_data_snapshot}"
 
 if [[ ${try} -eq 5 ]]; then
     echo "Backup failed"
