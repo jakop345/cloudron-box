da5b9f3abcd4804aa17477e146bf8c0d9ba457c5
Johannes Zellner <johannes@nebulon.de>
2015-04-05 15:26:58 +0200

    Apparently TIMESTAMP(2) is not supported everywhere

--- migrations/20150404093025-apps-add-createdAt.js
+++ migrations/20150404093025-apps-add-createdAt.js
@@ -2,7 +2,7 @@ dbm = dbm || require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-    db.runSql('ALTER TABLE apps ADD COLUMN createdAt TIMESTAMP(2) NOT NULL DEFAULT CURRENT_TIMESTAMP', function (error) {
+    db.runSql('ALTER TABLE apps ADD COLUMN createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP', function (error) {
         if (error) console.error(error);
         callback(error);
     });
