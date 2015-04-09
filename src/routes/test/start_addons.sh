1f71063183ec4fc30f999b9b925879e38abb3ac8
Girish Ramakrishnan <girish@forwardbias.in>
2015-04-09 08:53:58 -0700

    Remove any existing directory from previous failed tests

--- src/routes/test/start_addons.sh
+++ src/routes/test/start_addons.sh
@@ -10,8 +10,10 @@ start_postgresql() {
     postgresql_vars="POSTGRESQL_ROOT_PASSWORD=${root_password}; POSTGRESQL_ROOT_HOST=172.17.0.0/255.255.0.0"
 
     if which boot2docker >/dev/null; then
+        boot2docker ssh "sudo rm -rf /tmp/postgresql_vars.sh"
         boot2docker ssh "echo \"${postgresql_vars}\" > /tmp/postgresql_vars.sh"
     else
+        rm -rf /tmp/postgresql_vars.sh
         echo "${postgresql_vars}" > /tmp/postgresql_vars.sh
     fi
 
@@ -24,8 +26,10 @@ start_mysql() {
     local mysql_vars="MYSQL_ROOT_PASSWORD=${root_password}; MYSQL_ROOT_HOST=172.17.0.0/255.255.0.0"
 
     if which boot2docker >/dev/null; then
+        boot2docker ssh "sudo rm -rf /tmp/mysql_vars.sh"
         boot2docker ssh "echo \"${mysql_vars}\" > /tmp/mysql_vars.sh"
     else
+        rm -rf /tmp/mysql_vars.sh
         echo "${mysql_vars}" > /tmp/mysql_vars.sh
     fi
 
