f39368bb99b24ff0516619e280773494f310c2be
Girish Ramakrishnan <girish@forwardbias.in>
2015-04-08 19:24:53 -0700

    Remove backup.log
    Part of #324

--- src/scripts/backupbox.sh
+++ src/scripts/backupbox.sh
@@ -12,8 +12,6 @@ if [[ $# == 1 && "$1" == "--check" ]]; then
     exit 0
 fi
 
-exec 1>> "/var/log/cloudron/backup.log" 2>&1
-
 if [ $# -lt 2 ]; then
     echo "Usage: backupbox.sh <url> <key>"
     exit 1
