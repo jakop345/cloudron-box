77cbc15f0105a478434e605c170a968603edd717
Johannes Zellner <johannes@nebulon.de>
2015-04-23 12:26:19 +0200

    Only allow admins to see the settings

--- webadmin/src/views/settings.js
+++ webadmin/src/views/settings.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('Application').controller('SettingsController', ['$scope', '$location', 'Client', function ($scope, $location, Client) {
+    Client.onReady(function () { if (!Client.getUserInfo().admin) $location.path('/'); });
+
     $scope.user = Client.getUserInfo();
     $scope.config = Client.getConfig();
 
