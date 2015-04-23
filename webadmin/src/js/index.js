c29a20d8d1ce318f501810b645a62cc012125207
Johannes Zellner <johannes@nebulon.de>
2015-04-23 12:51:17 +0200

    Add prettyDate filter

--- webadmin/src/js/index.js
+++ webadmin/src/js/index.js
@@ -111,6 +111,28 @@ app.filter('prettyHref', function () {
     };
 });
 
+app.filter('prettyDate', function () {
+    // http://ejohn.org/files/pretty.js
+    return function prettyDate(time){
+        var date = new Date(time),
+            diff = (((new Date()).getTime() - date.getTime()) / 1000),
+            day_diff = Math.floor(diff / 86400);
+
+        if (isNaN(day_diff) || day_diff < 0 || day_diff >= 31)
+            return;
+
+        return day_diff == 0 && (
+                diff < 60 && 'just now' ||
+                diff < 120 && '1 minute ago' ||
+                diff < 3600 && Math.floor( diff / 60 ) + ' minutes ago' ||
+                diff < 7200 && '1 hour ago' ||
+                diff < 86400 && Math.floor( diff / 3600 ) + ' hours ago') ||
+            day_diff == 1 && 'Yesterday' ||
+            day_diff < 7 && day_diff + ' days ago' ||
+            day_diff < 31 && Math.ceil( day_diff / 7 ) + ' weeks ago';
+    };
+});
+
 // custom directive for dynamic names in forms
 // See http://stackoverflow.com/questions/23616578/issue-registering-form-control-with-interpolated-name#answer-23617401
 app.directive('laterName', function () {                   // (2)
