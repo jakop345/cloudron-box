11aecac6fde56f2289d711e4dee60baa1ccf4d00
Girish Ramakrishnan <girish@forwardbias.in>
2015-04-24 16:52:21 -0700

    Fix typo

--- setup/start/setup_infra.sh
+++ setup/start/setup_infra.sh
@@ -46,7 +46,7 @@ echo "Mail container id: ${mail_container_id}"
 mysql_addon_root_password=$(pwgen -1 -s)
 docker0_ip=$(/sbin/ifconfig docker0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
 cat > "${DATA_DIR}/addons/mysql_vars.sh" <<EOF
-readonly MYSQL_ROOT_PASSWORD='${mysql_root_password}'
+readonly MYSQL_ROOT_PASSWORD='${mysql_addon_root_password}'
 readonly MYSQL_ROOT_HOST='${docker0_ip}'
 EOF
 mysql_container_id=$(docker run --restart=always -d --name="mysql" \
@@ -59,7 +59,7 @@ echo "MySQL container id: ${mysql_container_id}"
 # postgresql
 postgresql_addon_root_password=$(pwgen -1 -s)
 cat > "${DATA_DIR}/addons/postgresql_vars.sh" <<EOF
-readonly POSTGRESQL_ROOT_PASSWORD='${postgresql_root_password}'
+readonly POSTGRESQL_ROOT_PASSWORD='${postgresql_addon_root_password}'
 EOF
 postgresql_container_id=$(docker run --restart=always -d --name="postgresql" \
     -h "${arg_fqdn}" \
@@ -71,7 +71,7 @@ echo "PostgreSQL container id: ${postgresql_container_id}"
 # mongodb
 mongodb_addon_root_password=$(pwgen -1 -s)
 cat > "${DATA_DIR}/addons/mongodb_vars.sh" <<EOF
-readonly MONGODB_ROOT_PASSWORD='${mongodb_root_password}'
+readonly MONGODB_ROOT_PASSWORD='${mongodb_addon_root_password}'
 EOF
 mongodb_container_id=$(docker run --restart=always -d --name="mongodb" \
     -h "${arg_fqdn}" \
