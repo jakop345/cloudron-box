95ccd4d4cbda46e0bc8595097d6424f46fae953c
Johannes Zellner <johannes@nebulon.de>
2015-05-28 09:48:13 +0200

    Add backups.js

---
+++ src/backups.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var assert = require('assert'),
+    util = require('util'),
+    config = require('../config.js'),
+    superagent = require('superagent');
+
+exports = module.exports = {
+    BackupsError: BackupsError,
+
+    getAll: getAll
+};
+
+function BackupsError(reason, errorOrMessage) {
+    assert(typeof reason === 'string');
+    assert(errorOrMessage instanceof Error || typeof errorOrMessage === 'string' || typeof errorOrMessage === 'undefined');
+
+    Error.call(this);
+    Error.captureStackTrace(this, this.constructor);
+
+    this.name = this.constructor.name;
+    this.reason = reason;
+    if (typeof errorOrMessage === 'undefined') {
+        this.message = reason;
+    } else if (typeof errorOrMessage === 'string') {
+        this.message = errorOrMessage;
+    } else {
+        this.message = 'Internal error';
+        this.nestedError = errorOrMessage;
+    }
+}
+util.inherits(BackupsError, Error);
+BackupsError.EXTERNAL_ERROR = 'external error';
+
+function getAll(callback) {
+    assert(typeof callback === 'function');
+
+    if (config.LOCAL) return callback(null, []);    // skip this when running locally
+
+    var url = config.apiServerOrigin() + '/api/v1/boxes/' + config.fqdn() + '/backups';
+
+    superagent.get(url).query({ token: config.token() }).end(function (error, result) {
+        if (error) return callback(new BackupsError(BackupsError.EXTERNAL_ERROR, error));
+        if (result.statusCode !== 200) return callback(new BackupsError(BackupsError.EXTERNAL_ERROR, result.text));
+
+        return callback(null, result.body.backups);
+    });
+}
