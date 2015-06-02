28b90966e7eb8f09f3c907e632fba8fef15d2470
Johannes Zellner <johannes@cloudron.io>
2015-06-02 16:10:07 +0200

    Remove stale code in dns view

--- webadmin/src/views/dns.js
+++ webadmin/src/views/dns.js
@@ -3,46 +3,10 @@
 angular.module('Application').controller('DnsController', ['$scope', '$location', 'Client', function ($scope, $location, Client) {
     Client.onReady(function () { if (!Client.getUserInfo().admin) $location.path('/'); });
 
-    $scope.ready = false;
-    $scope.user = Client.getUserInfo();
-    $scope.config = Client.getConfig();
     $scope.certificateFile = null;
     $scope.certificateFileName = '';
     $scope.keyFile = null;
     $scope.keyFileName = '';
-
-    $scope.backup = function () {
-        $('#backupProgressModal').modal('show');
-        $scope.$parent.initialized = false;
-
-        Client.backup(function (error) {
-            if (error) console.error(error);
-
-            // now start query
-            function checkIfDone() {
-                Client.version(function (error) {
-                    if (error) return window.setTimeout(checkIfDone, 1000);
-
-                    $('#backupProgressModal').modal('hide');
-                    $scope.$parent.initialized = true;
-                });
-            }
-
-            window.setTimeout(checkIfDone, 5000);
-        });
-    };
-
-    $scope.update = function () {
-        $('#updateModal').modal('hide');
-
-        $scope.$parent.initialized = false;
-
-        Client.update(function (error) {
-            if (error) console.error(error);
-
-            window.location.href = '/update.html';
-        });
-    };
 
     document.getElementById('idCertificate').onchange = function (event) {
         $scope.$apply(function () {
@@ -59,26 +23,13 @@ angular.module('Application').controller('DnsController', ['$scope', '$location'
     };
 
     $scope.setCertificate = function () {
-        console.log('Will set the certificate');
-
         if (!$scope.certificateFile) return console.log('Certificate not set');
         if (!$scope.keyFile) return console.log('Key not set');
 
         Client.setCertificate($scope.certificateFile, $scope.keyFile, function (error) {
-            if (error) return console.log(error);
+            if (error) return console.error(error);
 
             window.setTimeout(window.location.reload.bind(window.location, true), 3000);
         });
     };
-
-    Client.onConfig(function () {
-        $scope.tokenInUse = Client._token;
-
-        Client.getApps(function (error, apps) {
-            if (error) console.error('Error loading app list');
-            $scope.apps = apps;
-
-            $scope.ready = true;
-        });
-    });
 }]);
