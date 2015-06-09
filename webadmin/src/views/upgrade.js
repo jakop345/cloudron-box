59358c1ad30e255cecc0158f3b159fb7df38719f
Johannes Zellner <johannes@cloudron.io>
2015-06-09 14:51:42 +0200

    Fill migration combobox with available sizes

--- webadmin/src/views/upgrade.js
+++ webadmin/src/views/upgrade.js
@@ -1,8 +1,21 @@
 'use strict';
 
-angular.module('Application').controller('UpgradeController', ['$scope', '$location', 'Client', function ($scope, $location, Client) {
+angular.module('Application').controller('UpgradeController', ['$scope', '$location', 'Client', 'AppStore', function ($scope, $location, Client, AppStore) {
     Client.onReady(function () { if (!Client.getUserInfo().admin) $location.path('/'); });
 
     $scope.user = Client.getUserInfo();
     $scope.config = Client.getConfig();
+    $scope.availableSizes = [];
+
+    $scope.appMigrate = {
+        sizeSlug: 'small'
+    };
+
+    Client.onReady(function () {
+        AppStore.getSizes(function (error, result) {
+            if (error) return console.error(error);
+            angular.copy(result, $scope.availableSizes);
+            $scope.appMigrate.sizeSlug = $scope.availableSizes[0].slug;
+        });
+    });
 }]);
