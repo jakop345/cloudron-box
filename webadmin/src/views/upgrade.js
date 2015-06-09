19fcce6afe3ffca67fadb341821ee2e76b5f71e4
Johannes Zellner <johannes@cloudron.io>
2015-06-09 18:45:21 +0200

    Send the restoreKey for migration

--- webadmin/src/views/upgrade.js
+++ webadmin/src/views/upgrade.js
@@ -6,11 +6,13 @@ angular.module('Application').controller('UpgradeController', ['$scope', '$locat
     $scope.user = Client.getUserInfo();
     $scope.config = Client.getConfig();
     $scope.availableSizes = [];
+    $scope.availableBackups = [];
     $scope.currentSize = 'small';
     $scope.currentRegion = 'sfo';
 
     $scope.migration = {
-        sizeSlug: 'small'
+        sizeSlug: 'small',
+        restoreKey: null
     };
 
     $scope.showMigrationConfirm = function () {
@@ -18,17 +20,25 @@ angular.module('Application').controller('UpgradeController', ['$scope', '$locat
     };
 
     $scope.doMigration = function () {
-        Client.migrate($scope.migration.sizeSlug, function (error) {
+        Client.migrate($scope.migration.sizeSlug, $scope.migration.restoreKey, function (error) {
             if (error) return console.error(error);
             $('#migrationModal').modal('hide');
         });
     };
 
     Client.onReady(function () {
-        AppStore.getSizes(function (error, result) {
+        Client.getBackups(function (error, backups) {
             if (error) return console.error(error);
-            angular.copy(result, $scope.availableSizes);
-            $scope.migration.sizeSlug = $scope.availableSizes[0].slug;
+
+            angular.copy(backups, $scope.availableBackups);
+
+            AppStore.getSizes(function (error, result) {
+                if (error) return console.error(error);
+
+                angular.copy(result, $scope.availableSizes);
+
+                $scope.migration.sizeSlug = $scope.availableSizes[0].slug;
+            });
         });
     });
 }]);
