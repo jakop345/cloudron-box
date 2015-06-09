f41ac7a55571e5917b7793cf522ed5e9a7ed717d
Johannes Zellner <johannes@cloudron.io>
2015-06-09 22:27:13 +0200

    Add appstore.getRegions()

--- webadmin/src/js/appstore.js
+++ webadmin/src/js/appstore.js
@@ -87,5 +87,16 @@ angular.module('Application').service('AppStore', ['$http', 'Client', function (
         });
     };
 
+    AppStore.prototype.getRegions = function (callback) {
+        if (Client.getConfig().apiServerOrigin === null) return callback(new AppStoreError(420, 'Enhance Your Calm'));
+
+        $http.get(Client.getConfig().apiServerOrigin + '/api/v1/regions').success(function (data, status) {
+            if (status !== 200) return callback(new AppStoreError(status, data));
+            return callback(null, data.regions);
+        }).error(function (data, status) {
+            return callback(new AppStoreError(status, data));
+        });
+    };
+
     return new AppStore();
 }]);
