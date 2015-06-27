1e38caf2b2ead04745600ec7cefbd077f62cdddc
Girish Ramakrishnan <girish@cloudron.io>
2015-06-26 19:06:48 -0700

    start mongodb

--- src/routes/test/start_addons.sh
+++ src/routes/test/start_addons.sh
@@ -4,6 +4,7 @@ set -eu -o pipefail
 
 readonly mysqldatadir="/tmp/mysqldata-$(date +%s)"
 readonly postgresqldatadir="/tmp/postgresqldata-$(date +%s)"
+readonly mongodbdatadir="/tmp/mongodbdata-$(date +%s)"
 root_password=secret
 
 start_postgresql() {
@@ -38,8 +39,25 @@ start_mysql() {
     docker run -dP --name=mysql -v "${mysqldatadir}:/var/lib/mysql" -v /tmp/mysql_vars.sh:/etc/mysql/mysql_vars.sh cloudron/mysql:0.3.0 >/dev/null
 }
 
+start_mongodb() {
+    local mongodb_vars="MONGODB_ROOT_PASSWORD=${root_password}"
+
+    if which boot2docker >/dev/null; then
+        boot2docker ssh "sudo rm -rf /tmp/mongodb_vars.sh"
+        boot2docker ssh "echo \"${mongodb_vars}\" > /tmp/mongodb_vars.sh"
+    else
+        rm -rf /tmp/mongodb_vars.sh
+        echo "${mongodb_vars}" > /tmp/mongodb_vars.sh
+    fi
+
+    docker rm -f mongodb 2>/dev/null 1>&2 || true
+
+    docker run -dP --name=mongodb -v "${mongodbdatadir}:/var/lib/mongodb" -v /tmp/mongodb_vars.sh:/etc/mongodb_vars.sh cloudron/mongodb:0.3.0 >/dev/null
+}
+
 start_mysql
 start_postgresql
+start_mongodb
 
 echo -n "Waiting for addons to start"
 for i in {1..10}; do
