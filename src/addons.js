384576434116388283e7b1297d34ef88fbe9604f
Girish Ramakrishnan <girish@cloudron.io>
2015-07-02 00:45:46 -0700

    Use mailUsername instead for naked domain apps

--- src/addons.js
+++ src/addons.js
@@ -323,7 +323,7 @@ function setupSendMail(app, callback) {
     var env = [
         'MAIL_SMTP_SERVER=mail',
         'MAIL_SMTP_PORT=25',
-        'MAIL_SMTP_USERNAME=' + (app.location || 'nodomain'),
+        'MAIL_SMTP_USERNAME=' + (app.location || config.get('mailUsername')),
         'MAIL_DOMAIN=' + config.fqdn()
     ];
 
