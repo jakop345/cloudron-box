ed05f1ee46149a3abca9be41e1e779134ef8545d
Girish Ramakrishnan <girish@cloudron.io>
2015-07-15 10:47:51 -0700

    Fix failing settings test

--- src/test/database-test.js
+++ src/test/database-test.js
@@ -909,11 +909,12 @@ describe('database', function () {
             settingsdb.getAll(function (error, result) {
                 expect(error).to.be(null);
                 expect(result).to.be.an(Array);
-                expect(result.length).to.be(3);
+                expect(result.length).to.be(4);
                 expect(result[0].name).to.be('autoupdate_pattern');
                 expect(result[0].value).to.be.ok();
-                expect(result[1]).to.eql({ name: 'somekey', value: 'somevalue' });
-                expect(result[2].name).to.be('time_zone');
+                expect(result[1].name).to.be('cloudron_name');
+                expect(result[2]).to.eql({ name: 'somekey', value: 'somevalue' });
+                expect(result[3].name).to.be('time_zone');
                 done();
             });
         });
