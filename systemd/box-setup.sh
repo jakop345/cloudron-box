9b97e26b58e256bb237a39e7e45693c7f0ecbf64
Girish Ramakrishnan <girish@cloudron.io>
2015-08-26 12:15:47 -0700

    Use fallocate everywhere

--- systemd/box-setup.sh
+++ systemd/box-setup.sh
@@ -7,17 +7,18 @@ readonly BACKUP_SWAP_FILE="/backup.swap" # used when doing app backups
 readonly USER_HOME_FILE="/root/user_home.img"
 readonly DOCKER_DATA_FILE="/root/docker_data.img"
 
+# all sizes are in mb
 readonly physical_memory=$(free -m | awk '/Mem:/ { print $2 }')
 readonly app_count=$((${physical_memory} / 200)) # estimated app count
 readonly docker_data_size=$((6 * 1024 + app_count * 500)) # 6gb base + 500m for each app
 readonly disk_size_gb=$(fdisk -l /dev/vda1 | grep 'Disk /dev/vda1' | awk '{ print $3 }')
-readonly disk_size_mb=$((disk_size_gb * 1024))
-readonly backup_swap=1024
+readonly disk_size=$((disk_size_gb * 1024))
+readonly backup_swap_size=1024
 
 # Allocate two sets of swap files - one for general app usage and another for backup
 # The backup swap is setup for swap on the fly by the backup scripts
 if [[ ! -f "${APPS_SWAP_FILE}" ]]; then
-    echo "Creating Apps swap file of size ${physical_memory}m"
+    echo "Creating Apps swap file of size ${physical_memory}M"
     fallocate -l "${physical_memory}m" "${APPS_SWAP_FILE}"
     chmod 600 "${APPS_SWAP_FILE}"
     mkswap "${APPS_SWAP_FILE}"
@@ -28,8 +29,8 @@ else
 fi
 
 if [[ ! -f "${BACKUP_SWAP_FILE}" ]]; then
-    echo "Creating Backup swap file"
-    fallocate -l "${backup_swap}m" "${BACKUP_SWAP_FILE}"
+    echo "Creating Backup swap file of size ${backup_swap_size}M"
+    fallocate -l "${backup_swap_size}m" "${BACKUP_SWAP_FILE}"
     chmod 600 "${BACKUP_SWAP_FILE}"
     mkswap "${BACKUP_SWAP_FILE}"
 else
@@ -37,14 +38,14 @@ else
 fi
 
 if [ ! -f "${DOCKER_DATA_FILE}" ]; then
-    echo "Settings up btrfs docker"
     if aufs_mounts=$(grep 'aufs' /proc/mounts | awk '{ print $2 }' | sort -r); then
         umount -l "${aufs_mounts}"
     fi
     rm -rf /var/lib/docker
     mkdir /var/lib/docker
 
-    truncate -s "${docker_data_size}M" "${DOCKER_DATA_FILE}"
+    echo "Settings up btrfs docker of size ${docker_data_size}M"
+    fallocate -l "${docker_data_size}m" "${DOCKER_DATA_FILE}"
     mkfs.btrfs -L DockerData "${DOCKER_DATA_FILE}"
     echo "${DOCKER_DATA_FILE} /var/lib/docker btrfs loop,nosuid 0 0" >> /etc/fstab
     echo 'DOCKER_OPTS="-s btrfs"' >> /etc/default/docker
@@ -54,10 +55,9 @@ else
 fi
 
 if [[ ! -f "${USER_HOME_FILE}" ]]; then
-    echo "Seting up btrfs user home"
-    # create a separate 12GB fs for data
-    home_data_size=$((disk_size_mb - 2048 - docker_data_size - physical_memory - backup_swap))
-    truncate -s "${home_data_size}M" "${USER_HOME_FILE}"
+    home_data_size=$((disk_size - 2048 - docker_data_size - physical_memory - backup_swap_size))
+    echo "Setting up btrfs user home of size ${home_data_size}M"
+    fallocate -l "${home_data_size}m" "${USER_HOME_FILE}"
     mkfs.btrfs -L UserHome "${USER_HOME_FILE}"
     echo "${USER_HOME_FILE} ${USER_HOME} btrfs loop,nosuid 0 0" >> /etc/fstab
     mount "${USER_HOME_FILE}"
