335de765f3d7e51c04ad50b9eaff1097b051f64b
Johannes Zellner <johannes@cloudron.io>
2015-11-09 09:56:08 +0100

    Only show dns and cert pages for custom domain cloudrons

--- webadmin/src/views/certs.js
+++ webadmin/src/views/certs.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('Application').controller('CertsController', ['$scope', '$location', 'Client', function ($scope, $location, Client) {
-    Client.onReady(function () { if (!Client.getUserInfo().admin) $location.path('/'); });
+    Client.onReady(function () { if (!Client.getUserInfo().admin || !Client.getConfig().isCustomDomain) $location.path('/'); });
 
     $scope.defaultCert = {
         error: null,
