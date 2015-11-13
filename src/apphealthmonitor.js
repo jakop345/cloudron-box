d89db24bfc9aaa2e56b87a78da487fcd05ca2f85
Johannes Zellner <johannes@cloudron.io>
2015-11-13 09:30:33 +0100

    Fix indentantion

--- src/apphealthmonitor.js
+++ src/apphealthmonitor.js
@@ -110,7 +110,7 @@ function processApps(callback) {
         async.each(apps, checkAppHealth, function (error) {
             if (error) console.error(error);
 
-            var alive =apps
+            var alive = apps
                 .filter(function (a) { return a.installationState === appdb.ISTATE_INSTALLED && a.runState === appdb.RSTATE_RUNNING && a.health === appdb.HEALTH_HEALTHY; })
                 .map(function (a) { return a.location; }).join(', ');
 
