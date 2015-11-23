ba4edc5c0ef9601c73fed0c6d39dc559ee45ccf1
Girish Ramakrishnan <girish@cloudron.io>
2015-11-23 11:01:52 -0800

    implement some vultr api

--- images/vultr.js
+++ images/vultr.js
@@ -35,16 +35,50 @@ function getSshKeyId(keyName, callback) {
     callback(new Error('key not found'));
 }
 
+function create(keyId, name, callback) {
+    var regionId = 5; // LA (https://api.vultr.com/v1/regions/list)
+    var planId = 29;  // 768MB RAM (https://api.vultr.com/v1/regions/list)
+    var osid = 191; // Ubuntu 15.04 x64 (see https://api.vultr.com/v1/os/list)
+
+    var res = request.post('https://api.vultr.com/v1/server/create')
+        .query({ api_key : gApiToken })
+        .type('form')
+        .send({ DCID: regionId, VPSPLANID: planId, OSID : osid, label: name, SSHKEYID: keyId })
+        .end();
+
+    if (res.statusCode !== 200) return callback(new Error('Invalid response creating server'));
+
+    return callback(null, res.body.SUBID);
+}
+
+function getIp(id, callback){
+    var res = request.post('https://api.vultr.com/v1/server/list')
+        .query({ api_key : gApiToken })
+        .end();
+
+    var info = res.body[id];
+    if (!info) return callback(new Error('Invalid response querying IP'));
+
+    if (info.power_status !== 'running' || info.server_state !== 'ok' || info.status !== 'active') return callback(new Error('Server is not up yet'));
+
+    return callback(null, info.main_ip);
+}
+
 switch (process.argv[2]) {
 case 'get_ssh_key_id':
     getSshKeyId(process.argv[3], exit);
     break;
 
 case 'create':
+    create(process.argv[3], process.argv[4], exit);
+    break;
+
+case 'get_ip':
+    getIp(process.argv[3], exit);
+    break;
 
 case 'get_id':
 
-case 'get_ip':
 
 case 'power_on':
 
