640a0b2627c2dd44986beaf66247cc6f682f7bec
Johannes Zellner <johannes@cloudron.io>
2016-01-02 15:30:01 +0100

    Try to get the latest box release if no sourceTarballUrl is specified in the provisioning data

--- src/installer.js
+++ src/installer.js
@@ -6,6 +6,9 @@ var assert = require('assert'),
     child_process = require('child_process'),
     debug = require('debug')('installer:installer'),
     path = require('path'),
+    safe = require('safetydance'),
+    semver = require('semver'),
+    superagent = require('superagent'),
     util = require('util');
 
 exports = module.exports = {
@@ -72,19 +75,52 @@ function retire(args, callback) {
     spawn('retire', SUDO, pargs, callback);
 }
 
+function ensureVersion(args, callback) {
+    assert.strictEqual(typeof args, 'Object');
+    assert.strictEqual(typeof callback, 'function');
+
+    if (args.sourceTarballUrl) return callback(null, args.sourceTarballUrl);
+
+    if (!args.data.boxVersionsUrl) return callback(new Error('No boxVersionsUrl specified'));
+
+    superagent.get(args.data.boxVersionsUrl).end(function (error, result) {
+        if (error && !error.response) return callback(error);
+        if (result.statusCode !== 200) return callback(new Error(util.format('Bad status: %s %s', result.statusCode, result.text)));
+
+        var versions = safe.JSON.parse(result.text);
+
+        if (!versions || typeof versions !== 'object') return callback(new Error('versions is not in valid format:' + safe.error));
+
+        var latestVersion = Object.keys(versions).sort(semver.compare).pop();
+        debug('ensureVersion: Latest version is %s etag:%s', latestVersion, result.header['etag']);
+
+        if (!versions[latestVersion]) return callback(new Error('No version available'));
+        if (!versions[latestVersion].sourceTarballUrl) return callback(new Error('No sourceTarballUrl specified'));
+
+        args.sourceTarballUrl = versions[latestVersion].sourceTarballUrl;
+        args.data.version = latestVersion;
+
+        callback(null, args);
+    });
+}
+
 function provision(args, callback) {
     assert.strictEqual(typeof args, 'object');
     assert.strictEqual(typeof callback, 'function');
 
-    var pargs = [ INSTALLER_CMD ];
-    pargs.push('--sourcetarballurl', args.sourceTarballUrl);
-    pargs.push('--data', JSON.stringify(args.data));
+    ensureVersion(args, function (error, result) {
+        if (error) return callback(error);
 
-    debug('provision: calling with args %j', pargs);
+        var pargs = [ INSTALLER_CMD ];
+        pargs.push('--sourcetarballurl', result.sourceTarballUrl);
+        pargs.push('--data', JSON.stringify(result.data));
 
-    if (process.env.NODE_ENV === 'test') return callback(null);
+        debug('provision: calling with args %j', pargs);
 
-    // sudo is required for update()
-    spawn('provision', SUDO, pargs, callback);
+        if (process.env.NODE_ENV === 'test') return callback(null);
+
+        // sudo is required for update()
+        spawn('provision', SUDO, pargs, callback);
+    });
 }
 
