439b8923b890119a9ac08ec0b9f1a3ffc62129c1
Johannes Zellner <johannes@cloudron.io>
2016-01-05 14:33:33 +0100

    Implement ec2 sysinfo backend

--- src/sysinfo/ec2.js
+++ src/sysinfo/ec2.js
@@ -5,10 +5,17 @@ exports = module.exports = {
 };
 
 var assert = require('assert'),
-    SysInfoError = require('../sysinfo.js').SysInfoError;
+    superagent = require('superagent'),
+    SysInfoError = require('../sysinfo.js').SysInfoError,
+    util = require('util');
 
 function getIp(callback) {
     assert.strictEqual(typeof callback, 'function');
 
-    callback(new SysInfoError(SysInfoError.INTERNAL_ERROR, 'Not implemented'));
+    superagent.get('http://169.254.169.254/latest/meta-data/public-ipv4').end(function (error, result) {
+        if (error) return callback(new SysInfoError(SysInfoError.INTERNAL_ERROR, error.status ? 'Request failed: ' + error.status : 'Network failure'));
+        if (result.statusCode !== 200) return callback(new SysInfoError(SysInfoError.EXTERNAL_ERROR, util.format('%s %j', result.status, result.body)));
+
+        callback(null, result.text);
+    });
 }
