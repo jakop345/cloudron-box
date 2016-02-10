bd62efcff5f8da42581ca60cdb2af567e20445c2
Johannes Zellner <johannes@cloudron.io>
2016-02-10 15:01:51 +0100

    That api is of course a PUT api

--- webadmin/src/js/client.js
+++ webadmin/src/js/client.js
@@ -400,8 +400,8 @@ angular.module('Application').service('Client', ['$http', 'md5', 'Notification',
     };
 
     Client.prototype.setGroups = function (userId, groupIds, callback) {
-        $http.get(client.apiOrigin + '/api/v1/users/' + userId + '/set_groups').success(function (data, status) {
-            if (status !== 200 || typeof data !== 'object') return callback(new ClientError(status, data));
+        $http.put(client.apiOrigin + '/api/v1/users/' + userId + '/set_groups', { groupIds: groupIds }).success(function (data, status) {
+            if (status !== 204) return callback(new ClientError(status, data));
             callback(null);
         }).error(defaultErrorHandler(callback));
     };
