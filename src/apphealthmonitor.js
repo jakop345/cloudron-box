9bf1fe3b7df55d19ec3b6e59a73eac727ab5bde0
Johannes Zellner <johannes@cloudron.io>
2016-02-14 17:42:52 +0100

    Show naked_domain for healthtask summary

--- src/apphealthmonitor.js
+++ src/apphealthmonitor.js
@@ -116,7 +116,7 @@ function processApps(callback) {
 
             var alive = apps
                 .filter(function (a) { return a.installationState === appdb.ISTATE_INSTALLED && a.runState === appdb.RSTATE_RUNNING && a.health === appdb.HEALTH_HEALTHY; })
-                .map(function (a) { return a.location; }).join(', ');
+                .map(function (a) { return a.location || 'naked_domain'; }).join(', ');
 
             debug('apps alive: [%s]', alive);
 
