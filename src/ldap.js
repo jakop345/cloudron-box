0f2037513bd153d63c514f4c233776321105ca80
Girish Ramakrishnan <girish@cloudron.io>
2016-05-12 21:48:42 -0700

    remove recvmail bind

--- src/ldap.js
+++ src/ldap.js
@@ -192,12 +192,6 @@ function start(callback) {
         res.end();
     });
 
-    // this is the bind for the recvmail addon to authorize apps and users
-    gServer.bind('ou=sendmail,dc=cloudron', function(req, res, next) {
-        debug('recvmail bind: %s', req.dn.toString()); // note: cn can be email or id
-        userBind(req, res, next); // TODO: extend this for apps that require imap addon
-    });
-
     // this is the bind for addons (after bind, they might search and authenticate)
     gServer.bind('ou=addons,dc=cloudron', function(req, res, next) {
         debug('addons bind: %s', req.dn.toString()); // note: cn can be email or id
