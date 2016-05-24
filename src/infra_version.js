2072dedf661aaea3a06f7efacb97d5f8ffacf7c4
Girish Ramakrishnan <girish@cloudron.io>
2016-05-24 16:46:54 -0700

    bump mail addon version

--- src/infra_version.js
+++ src/infra_version.js
@@ -5,7 +5,7 @@
 // Do not require anything here!
 
 exports = module.exports = {
-    'version': 33,
+    'version': 34,
 
     'baseImage': 'cloudron/base:0.8.1',
 
@@ -14,7 +14,7 @@ exports = module.exports = {
         'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:0.10.0' },
         'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:0.9.0' },
         'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:0.8.0' },
-        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.13.0' },
+        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.13.1' },
         'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.8.0' }
     }
 };
