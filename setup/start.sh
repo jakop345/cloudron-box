5df3a4198844a99add90b57f4fa150bf4d1b20b4
Girish Ramakrishnan <girish@cloudron.io>
2016-05-30 14:47:33 -0700

    INFRA_VERSION may not exist

--- setup/start.sh
+++ setup/start.sh
@@ -119,7 +119,8 @@ else
 fi
 
 set_progress "33" "Changing ownership"
-chown "${USER}:${USER}" -R "${DATA_DIR}/box" "${DATA_DIR}/nginx" "${DATA_DIR}/collectd" "${DATA_DIR}/addons" "${DATA_DIR}/acme" "${DATA_DIR}/INFRA_VERSION"
+chown "${USER}:${USER}" -R "${DATA_DIR}/box" "${DATA_DIR}/nginx" "${DATA_DIR}/collectd" "${DATA_DIR}/addons" "${DATA_DIR}/acme"
+chown "${DATA_DIR}/INFRA_VERSION" || true
 chown "${USER}:${USER}" "${DATA_DIR}"
 
 set_progress "65" "Creating cloudron.conf"
