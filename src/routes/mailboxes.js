8d65f93fa431a55bdd418dbe67338410022cbdb8
Girish Ramakrishnan <girish@cloudron.io>
2016-06-02 14:40:29 -0700

    return error.message

--- src/routes/mailboxes.js
+++ src/routes/mailboxes.js
@@ -71,7 +71,7 @@ function setAliases(req, res, next) {
 
     mailboxes.setAliases(req.params.mailboxId, req.body.aliases, function (error) {
         if (error && error.reason === MailboxError.NOT_FOUND) return next(new HttpError(404, 'No such mailbox'));
-        if (error && error.reason === MailboxError.BAD_FIELD) return next(new HttpError(400, error.reason));
+        if (error && error.reason === MailboxError.BAD_FIELD) return next(new HttpError(400, error.message));
         if (error && error.reason === MailboxError.ALREADY_EXISTS) return next(new HttpError(409, 'One or more alias already exist'));
         if (error) return next(new HttpError(500, error));
 
