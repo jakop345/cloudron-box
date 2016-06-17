dbadbd2c4e2a956b552616e61886b0fbc19dfa8b
Girish Ramakrishnan <girish@cloudron.io>
2016-06-16 22:00:18 -0500

    bump all the container versions

--- src/infra_version.js
+++ src/infra_version.js
@@ -5,17 +5,17 @@
 // Do not require anything here!
 
 exports = module.exports = {
-    'version': 36,
+    'version': 37,
 
     'baseImage': 'cloudron/base:0.8.1',
 
     'images': {
-        'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:0.11.0' },
+        'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:0.12.0' },
         'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:0.11.0' },
-        'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:0.9.0' },
-        'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:0.8.0' },
-        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.13.2' },
-        'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.8.0' }
+        'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:0.10.0' },
+        'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:0.9.0' },
+        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.14.0' },
+        'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.9.0' }
     }
 };
 
