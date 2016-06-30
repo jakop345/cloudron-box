9182038d127ffd24bd2db5cf890164f1267c8f0e
Johannes Zellner <johannes@cloudron.io>
2016-06-30 15:01:32 +0200

    Revert "Do not wait for cloudron.target to stop"
    This reverts commit dcfe2e4fdbcbd2acb98cefe9b50ef0bb1828eb48.

--- setup/stop.sh
+++ setup/stop.sh
@@ -4,5 +4,4 @@ set -eu -o pipefail
 
 echo "Stopping cloudron"
 
-# we do not wait until all services stop, so we can still respond to the retire request
-systemctl stop --no-block cloudron.target
+systemctl stop cloudron.target
