14983861c0ba7e2f5a40c07703dfebe885497294
Johannes Zellner <johannes@cloudron.io>
2016-06-30 10:52:19 +0200

    Do not wait for cloudron.target to stop
    This will allow the box code to respond properly to the retire request

--- setup/stop.sh
+++ setup/stop.sh
@@ -4,4 +4,5 @@ set -eu -o pipefail
 
 echo "Stopping cloudron"
 
-systemctl stop cloudron.target
+# we do not wait until all services stop, so we can still respond to the retire request
+systemctl stop --no-block cloudron.target
