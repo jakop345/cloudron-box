ebb564f623a91b8f2408798fa646a4f33c817967
Johannes Zellner <johannes@cloudron.io>
2016-07-14 12:46:52 +0200

    Add tooltip to show the exact time in event log

--- webadmin/src/js/index.js
+++ webadmin/src/js/index.js
@@ -9,7 +9,7 @@ if (search.accessToken) localStorage.token = search.accessToken;
 
 
 // create main application module
-var app = angular.module('Application', ['ngRoute', 'ngAnimate', 'ngSanitize', 'angular-md5', 'slick', 'ui-notification', 'ui.bootstrap-slider']);
+var app = angular.module('Application', ['ngRoute', 'ngAnimate', 'ngSanitize', 'angular-md5', 'slick', 'ui-notification', 'ui.bootstrap', 'ui.bootstrap-slider']);
 
 app.config(['NotificationProvider', function (NotificationProvider) {
     NotificationProvider.setOptions({
