451c770b5cdc149caeb6b5624b5162c32826a9f5
Girish Ramakrishnan <girish@cloudron.io>
2016-08-02 10:40:17 -0700

    ACME agreement url has changed

--- src/cert/acme.js
+++ src/cert/acme.js
@@ -16,7 +16,7 @@ var assert = require('assert'),
 
 var CA_PROD = 'https://acme-v01.api.letsencrypt.org',
     CA_STAGING = 'https://acme-staging.api.letsencrypt.org',
-    LE_AGREEMENT = 'https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf';
+    LE_AGREEMENT = 'https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf';
 
 exports = module.exports = {
     getCertificate: getCertificate,
