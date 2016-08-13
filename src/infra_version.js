d342652212b6accb265d5716200e3ce8d8162c5c
Girish Ramakrishnan <girish@cloudron.io>
2016-08-12 17:17:48 -0700

    bump mail container version (spam support)

--- src/infra_version.js
+++ src/infra_version.js
@@ -15,7 +15,7 @@ exports = module.exports = {
         'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:0.11.0' },
         'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:0.10.0' },
         'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:0.9.0' },
-        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.18.0' },
+        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.19.0' },
         'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.9.0' }
     }
 };
