2ae4f76af5bf305a66d818c21586ba29a8217e6d
Girish Ramakrishnan <girish@cloudron.io>
2016-09-13 18:01:10 -0700

    x

--- webadmin/src/js/client.js
+++ webadmin/src/js/client.js
@@ -980,6 +980,8 @@ angular.module('Application').service('Client', ['$http', 'md5', 'Notification',
         var totalMemory = roundedMemory * 1.2; // box-setup.sh creates equal amount of swap. 1.2 factor is arbitrary
         var available = (totalMemory || 0) - used;
 
+        console.log(needed, used, roundedMemory, totalMemory, available);
+
         return (available - needed) >= 0;
     };
 
