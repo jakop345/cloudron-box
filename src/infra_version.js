1a25ad77ca5957fcd1d4e4dd2ea942e19c82ec77
Girish Ramakrishnan <girish@cloudron.io>
2016-10-03 13:53:11 -0700

    use latest mail container

--- src/infra_version.js
+++ src/infra_version.js
@@ -17,7 +17,7 @@ exports = module.exports = {
         'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:0.13.0' },
         'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:0.11.0' },
         'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:0.10.0' },
-        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.21.0' },
+        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.22.0' },
         'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.10.0' }
     }
 };
