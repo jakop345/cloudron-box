4ed74a8164ca403fb0eff6c99a82820312f8f323
Girish Ramakrishnan <girish@cloudron.io>
2016-10-09 12:53:55 -0700

    bump postgresql and mail images

--- src/infra_version.js
+++ src/infra_version.js
@@ -14,10 +14,10 @@ exports = module.exports = {
     // This is because we upgrade using dumps instead of mysql_upgrade, pg_upgrade etc
     'images': {
         'mysql': { repo: 'cloudron/mysql', tag: 'cloudron/mysql:0.13.0' },
-        'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:0.14.0' },
+        'postgresql': { repo: 'cloudron/postgresql', tag: 'cloudron/postgresql:0.15.0' },
         'mongodb': { repo: 'cloudron/mongodb', tag: 'cloudron/mongodb:0.11.0' },
         'redis': { repo: 'cloudron/redis', tag: 'cloudron/redis:0.10.0' },
-        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.22.0' },
+        'mail': { repo: 'cloudron/mail', tag: 'cloudron/mail:0.23.0' },
         'graphite': { repo: 'cloudron/graphite', tag: 'cloudron/graphite:0.10.0' }
     }
 };
