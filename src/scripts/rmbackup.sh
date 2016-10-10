f05a5226bac4475ff9b9723cf59ec6ba11e378dd
Johannes Zellner <johannes@nebulon.de>
2016-10-10 16:10:34 +0200

    Add new sudo file rmbackup.sh as backups are owned by root currently

---
+++ src/scripts/rmbackup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+if [[ ${EUID} -ne 0 ]]; then
+    echo "This script should be run as root." > /dev/stderr
+    exit 1
+fi
+
+if [[ $# -eq 0 ]]; then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+if [[ "$1" == "--check" ]]; then
+    echo "OK"
+    exit 0
+fi
+
+# remove the file
+rm -rf "${1}"
