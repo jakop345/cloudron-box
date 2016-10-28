7b1b369e4008cb07640e1e45dabdd73a58419526
Johannes <johannes@cloudron.io>
2016-10-28 15:28:48 +0200

    Add select box for S3 region

--- webadmin/src/views/settings.js
+++ webadmin/src/views/settings.js
@@ -23,6 +23,22 @@ angular.module('Application').controller('SettingsController', ['$scope', '$loca
     $scope.availablePlans = [];
     $scope.currentPlan = null;
 
+    // List is from http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
+    $scope.s3Regions = [
+        { name: 'US East (N. Virginia)', value: 'us-east-1' },
+        { name: 'US East (Ohio)', value: 'us-east-2' },
+        { name: 'US West (N. California)', value: 'us-west-1' },
+        { name: 'US West (Oregon)', value: 'us-west-2' },
+        { name: 'Asia Pacific (Mumbai)', value: 'ap-south-1' },
+        { name: 'Asia Pacific (Seoul)', value: 'ap-northeast-2' },
+        { name: 'Asia Pacific (Singapore)', value: 'ap-southeast-1' },
+        { name: 'Asia Pacific (Sydney)', value: 'ap-southeast-2' },
+        { name: 'Asia Pacific (Tokyo)', value: 'ap-northeast-1' },
+        { name: 'EU (Frankfurt)', value: 'eu-central-1' },
+        { name: 'EU (Ireland)', value: 'eu-west-1' },
+        { name: 'South America (São Paulo)', value: 'sa-east-1' }
+    ];
+
     $scope.planChange = {
         busy: false,
         error: {},
