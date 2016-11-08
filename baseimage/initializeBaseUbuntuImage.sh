c9e7308f49a892cd1fa9ac41f22ae074543a594e
Johannes <johannes@cloudron.io>
2016-11-08 09:35:10 +0100

    Attempt to set kernel params for generic provider
    This is useful for running ubuntu on hardware or in virtualbox

--- baseimage/initializeBaseUbuntuImage.sh
+++ baseimage/initializeBaseUbuntuImage.sh
@@ -139,7 +139,7 @@ echo "=== Enable memory accounting =="
 if [[ "${PROVIDER}" == "digitalocean" ]] || [[ "${PROVIDER}" == "caas" ]]; then
     sed -e 's/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="console=tty1 root=LABEL=DOROOT notsc clocksource=kvm-clock net.ifnames=0 cgroup_enable=memory swapaccount=1 panic_on_oops=1 panic=5"/' -i /etc/default/grub
     update-grub
-elif [[ "${PROVIDER}" == "ec2" ]]; then
+elif [[ "${PROVIDER}" == "ec2" ]] || [[ "${PROVIDER}" == "generic" ]]; then
     sed -e 's/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1 panic_on_oops=1 panic=5"/' -i /etc/default/grub
     update-grub
 fi
