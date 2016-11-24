1c828f19a3a292d8682c42c61cda936356b7dad4
Johannes <johannes@cloudron.io>
2016-11-24 15:46:21 +0100

    Remove console.log()

--- webadmin/src/js/index.js
+++ webadmin/src/js/index.js
@@ -181,7 +181,6 @@ app.filter('applicationLink', function() {
 app.filter('configureLink', function() {
     return function(app) {
         if (app.manifest.configurePath && app.installationState === ISTATES.INSTALLED && app.health === HSTATES.HEALTHY) {
-            console.log('===', app.manifest.configurePath)
             return 'https://' + app.fqdn + (app.manifest.configurePath[0] !== '/' ? '/' : '') + app.manifest.configurePath;
         } else {
             return '';
