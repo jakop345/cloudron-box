816911d071dbf6e6a1f041fc715c17348e37b29f
Johannes Zellner <johannes@cloudron.io>
2016-12-07 10:33:16 +0100

    Make s3 backup scripts aware of endpoints
    Part of #123

--- src/scripts/backuptests3.sh
+++ src/scripts/backuptests3.sh
@@ -6,7 +6,14 @@ readonly s3_url="$1"
 export AWS_ACCESS_KEY_ID="$2"
 export AWS_SECRET_ACCESS_KEY="$3"
 export AWS_DEFAULT_REGION="$4"
+readonly endpoint_url="$5"
 
-echo "Test Content" | aws s3 cp - "${s3_url}"
+optional_args=""
 
-aws s3 rm "${s3_url}"
+if [ -n "${endpoint_url}" ]; then
+	optional_args="--endpoint-url ${endpoint_url}"
+fi
+
+echo "Test Content" | aws ${optional_args} s3 cp - "${s3_url}"
+
+aws ${optional_args} s3 rm "${s3_url}"
